1302d19d3576e04bf8d72d4d290509a7f50ea6c7
WhoAteMyCQQkie <128926564+WhoAteMyCQQkie@users.noreply.github.com>
2024-08-23 20:21:21 +0200

    Updated Suzuran

--- damagecalc/damage_formulas.py
+++ damagecalc/damage_formulas.py
@@ -11617,73 +11617,22 @@ class Surtr(Operator):
 
 class Suzuran(Operator):
 	def __init__(self, pp, lvl = 0, pot=-1, skill=-1, mastery = 3, module=-1, module_lvl = 3, targets=1, TrTaTaSkMo=[True,True,True,True,True], buffs=[0,0,0,0,0],**kwargs):
-		maxlvl=90
-		lvl1atk = 507  #######including trust
-		maxatk = 596
-		self.atk_interval = 1.9  #### in seconds
-		level = lvl if lvl > 0 and lvl < maxlvl else maxlvl
-		self.base_atk = lvl1atk + (maxatk-lvl1atk) * (level-1) / (maxlvl-1)
-		self.pot = pot if pot in range(1,7) else 1
-		if self.pot > 3: self.base_atk += 25
-		
-		self.skill = skill if skill in [1,2] else 2 ###### check implemented skills
-		self.mastery = mastery if mastery in [0,1,2,3] else 3
-		if level != maxlvl: self.name = f"Suzuran Lv{level} P{self.pot} S{self.skill}" #####set op name
-		else: self.name = f"Suzuran P{self.pot} S{self.skill}"
-		if self.mastery == 0: self.name += "L7"
-		elif self.mastery < 3: self.name += f"M{self.mastery}"
-		self.targets = max(1,targets)
-	
-		self.module = module if module in [0,1,2] else 1 ##### check valid modules
-		self.module_lvl = module_lvl if module_lvl in [1,2,3] else 3		
-		if level >= maxlvl-30:
-			if self.module == 1:
-				self.name += f" ModX{self.module_lvl}"
-			elif self.module == 2:
-				if self.module_lvl == 3: self.base_atk += 46
-				elif self.module_lvl == 2: self.base_atk += 43
-				else: self.base_atk += 38
-				self.name += f" ModY{self.module_lvl}"
-			else: self.name += " no Mod"
-		else: self.module = 0
-
+		super().__init__("Suzuran", pp, [1,2],[1,2],2,1,1)
 		if self.targets > 1 and self.skill == 2: self.name += f" {self.targets}targets" ######when op has aoe
-		
-		self.buffs = buffs
-			
 	
 	def skill_dps(self, defense, res):
-		dps = 0
-		atkbuff = self.buffs[0]
-		aspd = self.buffs[2]
-		atk_scale = 1
-		
-		#talent/module buffs
-		if self.module == 1:
-			aspd += 3 + self.module_lvl
-			if self.module_lvl > 1: atkbuff += 0.03 * self.module_lvl
-		
-		fragile = 0.23 if self.pot > 4 else 0.2
-		if self.module == 2: fragile += 0.01 * (self.module_lvl - 1)
+		atkbuff = self.skill_params[0]
+		try: atkbuff += self.talent1_params[1]
+		except: pass
+		aspd = self.skill_params[1] if self.skill == 1 else 0
+		fragile = self.talent2_params[0] - 1	
+		fragile = max(fragile, self.buff_fragile)
+		final_atk = self.atk * (1 + atkbuff + self.buff_atk) + self.buff_atk_flat
+		hitdmg = np.fmax(final_atk * (1-res/100), final_atk * 0.05)
+		dps = hitdmg / self.atk_interval * (self.attack_speed + aspd)/100
+		if self.skill == 2 and self.targets > 1: dps *= min(self.targets, self.skill_params[1])
 			
-		fragile = max(fragile, self.buffs[3])
-		
-		####the actual skills
-		if self.skill == 1:
-			atkbuff += 0.8 if self.mastery == 3 else 0.6 + 0.05 * self.mastery
-			aspd += 15 + 5 * self.mastery
-			final_atk = self.base_atk * (1+atkbuff) + self.buffs[1]
-			hitdmg = np.fmax(final_atk * (1-res/100), final_atk * 0.05)
-			dps = hitdmg/(self.atk_interval/(1+aspd/100))
-		
-		if self.skill == 2:
-			atkbuff += 0.3 if self.mastery == 0 else 0.15 + 0.15 * self.mastery
-			max_targets = 2 if self.mastery == 0 else 3
-			final_atk = self.base_atk * (1+atkbuff) + self.buffs[1]
-			hitdmg = np.fmax(final_atk * (1-res/100), final_atk * 0.05)
-			dps = hitdmg/(self.atk_interval/(1+aspd/100)) * min(self.targets, max_targets)
-			
-		return dps*(1+fragile)/(1+self.buffs[3])
+		return dps*(1+fragile)/(1+self.buff_fragile)
 
 class SwireAlt(Operator):
 	def __init__(self, pp, lvl = 0, pot=-1, skill=-1, mastery = 3, module=-1, module_lvl = 3, targets=1, TrTaTaSkMo=[True,True,True,True,True], buffs=[0,0,0],**kwargs):
