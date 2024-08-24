b7f9ed4247ce181b6db590ba46a69a193b3b8c81
WhoAteMyCQQkie <128926564+WhoAteMyCQQkie@users.noreply.github.com>
2024-08-24 13:01:32 +0200

    Updated Mostima

--- damagecalc/damage_formulas.py
+++ damagecalc/damage_formulas.py
@@ -8360,73 +8360,18 @@ class Morgan(Operator):
 
 class Mostima(Operator):
 	def __init__(self, pp, lvl = 0, pot=-1, skill=-1, mastery = 3, module=-1, module_lvl = 3, targets=1, TrTaTaSkMo=[True,True,True,True,True], buffs=[0,0,0,0,0],**kwargs):
-		maxlvl=90
-		lvl1atk = 805  #######including trust
-		maxatk = 939
-		self.atk_interval = 2.9   #### in seconds
-		level = lvl if lvl > 0 and lvl < maxlvl else maxlvl
-		self.base_atk = lvl1atk + (maxatk-lvl1atk) * (level-1) / (maxlvl-1)
-		self.pot = pot if pot in range(1,7) else 1
-		if self.pot > 3: self.base_atk += 34
-		
-		self.skill = skill if skill in [1,2,3] else 3 ###### check implemented skills
-		self.mastery = mastery if mastery in [0,1,2,3] else 3
-		if level != maxlvl: self.name = f"Mostima Lv{level} P{self.pot} S{self.skill}" #####set op name
-		else: self.name = f"Mostima P{self.pot} S{self.skill}"
-		if self.mastery == 0: self.name += "L7"
-		elif self.mastery < 3: self.name += f"M{self.mastery}"
-		self.targets = max(1,targets)
-
-		self.module = module if module in [0,1,2] else 2 ##### check valid modules
-		self.module_lvl = module_lvl if module_lvl in [1,2,3] else 3		
-		if level >= maxlvl-30:
-			if self.module == 1:
-				if self.module_lvl == 3: self.base_atk += 80
-				elif self.module_lvl == 2: self.base_atk += 68
-				else: self.base_atk += 55
-				self.name += f" ModX{self.module_lvl}"
-			elif self.module == 2:
-				if self.module_lvl == 3: self.base_atk += 82
-				elif self.module_lvl == 2: self.base_atk += 68
-				else: self.base_atk += 51
-				self.name += f" ModY{self.module_lvl}"
-			else: self.name += " no Mod"
-		else: self.module = 0
-		
-		if self.targets > 1: self.name += f" {self.targets}targets" ######when op has aoe
-		
-		self.buffs = buffs
+		super().__init__("Mostima", pp, [1,2,3], [2,1], 3, 1, 2)
+		if self.targets > 1: self.name += f" {self.targets}targets"
 			
 	def skill_dps(self, defense, res):
-		dps = 0
-		atkbuff = self.buffs[0]
-		aspd = self.buffs[2]
-		atk_scale = 1
-		
-		#talent/module buffs
-		if self.module == 2:
-			aspd += 4 + self.module_lvl
-			
-		####the actual skills
-		if self.skill == 1:
-			atkbuff += 0.9 if self.mastery == 3 else 0.6 + 0.15 * self.mastery
-			
-			final_atk = self.base_atk * (1+atkbuff) + self.buffs[1]
-			
-			hitdmg = np.fmax(final_atk * (1-res/100), final_atk * 0.05)
-			dps = hitdmg/(self.atk_interval/(1+aspd/100))
 		if self.skill == 2:
-			skill_scale = 1.1 + self.mastery * 0.1
-			if self.mastery < 2: skill_scale += 0.1
-			
-			final_atk = self.base_atk * (1+atkbuff) + self.buffs[1]
-			hitdmg = np.fmax(final_atk  * skill_scale * (1-res/100), final_atk * skill_scale * 0.05)
-			dps = hitdmg
-		if self.skill == 3:
-			atkbuff += 1.7 if self.mastery == 3 else 1.2 + 0.15 * self.mastery
-			final_atk = self.base_atk * (1+atkbuff) + self.buffs[1]
+			skill_scale = self.skill_params[0]
+			final_atk = self.atk * (1 + self.buff_atk) + self.buff_atk_flat
+			dps = np.fmax(final_atk  * skill_scale * (1-res/100), final_atk * skill_scale * 0.05)
+		if self.skill != 2:
+			final_atk = self.atk * (1 + self.buff_atk + self.skill_params[0]) + self.buff_atk_flat
 			hitdmg = np.fmax(final_atk * (1-res/100), final_atk * 0.05)
-			dps = hitdmg/(self.atk_interval/(1+aspd/100))
+			dps = hitdmg / self.atk_interval * self.attack_speed / 100
 		return dps * self.targets
 
 class Mountain(Operator):
