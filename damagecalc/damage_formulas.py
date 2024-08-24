0dbab801f6e129234f20f11c8dd1f068566c6f96
WhoAteMyCQQkie <128926564+WhoAteMyCQQkie@users.noreply.github.com>
2024-08-24 20:19:54 +0200

    Updated Tomimi

--- damagecalc/damage_formulas.py
+++ damagecalc/damage_formulas.py
@@ -11843,66 +11843,24 @@ class Toddifons(Operator):
 
 class Tomimi(Operator):
 	def __init__(self, pp, lvl = 0, pot=-1, skill=-1, mastery = 3, module=-1, module_lvl = 3, targets=1, TrTaTaSkMo=[True,True,True,True,True], buffs=[0,0,0],**kwargs):
-		maxlvl=80
-		lvl1atk = 539  #######including trust
-		maxatk = 635
-		self.atk_interval = 1.6   #### in seconds
-		level = lvl if lvl > 0 and lvl < maxlvl else maxlvl
-		self.base_atk = lvl1atk + (maxatk-lvl1atk) * (level-1) / (maxlvl-1)
-		self.pot = pot if pot in range(1,7) else 6
-		
-		self.skill = skill if skill in [1,2] else 2 ###### check implemented skills
-		self.mastery = mastery if mastery in [0,1,2,3] else 3
-		if level != maxlvl: self.name = f"Tomimi Lv{level} P{self.pot} S{self.skill}" #####set op name
-		else: self.name = f"Tomimi P{self.pot} S{self.skill}"
-		if self.mastery == 0: self.name += "L7"
-		elif self.mastery < 3: self.name += f"M{self.mastery}"
-		self.targets = max(1,targets)
-		
-		self.module = module if module in [0,2] else 2 ##### check valid modules
-		self.module_lvl = module_lvl if module_lvl in [1,2,3] else 3		
-		if level >= maxlvl-30:
-			if self.module == 2:
-				if self.module_lvl == 3: self.base_atk += 50
-				elif self.module_lvl == 2: self.base_atk += 40
-				else: self.base_atk += 30
-				self.name += f" ModY{self.module_lvl}"
-			else: self.name += " no Mod"
-		else: self.module = 0
-		
+		super().__init__("Tomimi", pp, [1,2], [2],2,6,2)
 		if self.targets > 1 and self.skill == 2: self.name += f" {self.targets}targets" ######when op has aoe
-		
-		self.buffs = buffs
 				
 	def skill_dps(self, defense, res):
-		dps = 0
-		atkbuff = self.buffs[0]
-		aspd = self.buffs[2]
-		atk_scale = 1
-		
-		#talent/module buffs
-		atkbuff += 1
-		if self.pot > 4: atkbuff += 0.2
-		if self.module == 2: atkbuff += 0.05 * (self.module_lvl -1)
-			
-		####the actual skills
+		atkbuff = self.talent1_params[0]
+		final_atk = self.atk * (1+atkbuff + self.buff_atk) + self.buff_atk_flat
+		hitdmg = np.fmax(final_atk - defense, final_atk * 0.05)
 		if self.skill == 1:
-			aspd += 60 + 10 * self.mastery
-			final_atk = self.base_atk * (1+atkbuff) + self.buffs[1]
-			hitdmg = np.fmax(final_atk - defense, final_atk * 0.05)
-			dps = hitdmg/(self.atk_interval/(1+aspd/100))
+			dps = hitdmg/self.atk_interval * (self.attack_speed + self.skill_params[0]) / 100
 		if self.skill == 2:
-			crate = 0.7 + 0.1 * self.mastery
-			atk_scale = 1.9 + 0.15 * self.mastery
-			if self.mastery == 3: atk_scale = 2.2
-			final_atk = self.base_atk * (1+atkbuff) + self.buffs[1]
-			hitdmg = np.fmax(final_atk - defense, final_atk * 0.05)
+			crate = self.skill_params[2]
+			atk_scale = self.skill_params[1]
 			critdmg = np.fmax(final_atk * atk_scale - defense, final_atk * atk_scale * 0.05)
 			avgnormal = (1-crate) * hitdmg
 			avgstun = crate / 3 * hitdmg
 			avgcrit = crate / 3 * critdmg
 			avgaoe = crate / 3 * hitdmg * self.targets
-			dps = (avgnormal + avgstun + avgcrit + avgaoe)/(self.atk_interval/(1+aspd/100))
+			dps = (avgnormal + avgstun + avgcrit + avgaoe)/self.atk_interval * self.attack_speed/100
 		return dps
 
 class Totter(Operator):
