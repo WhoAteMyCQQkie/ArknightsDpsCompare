dd4ba8adabd7f58c0e69c3dff9937df34e2cc078
WhoAteMyCQQkie <128926564+WhoAteMyCQQkie@users.noreply.github.com>
2024-08-26 00:09:10 +0200

    Updated Angelina

--- damagecalc/damage_formulas.py
+++ damagecalc/damage_formulas.py
@@ -760,83 +760,34 @@ class Andreana(Operator):
 		return dps
 
 class Angelina(Operator):
-	def __init__(self, pp, lvl = 0, pot=-1, skill=-1, mastery = 3, module=-1, module_lvl = 3, targets=1, TrTaTaSkMo=[True,True,True,True,True], buffs=[0,0,0],**kwargs):
-		maxlvl=90
-		lvl1atk = 524  #######including trust
-		maxatk = 617
-		self.atk_interval = 1.9   #### in seconds
-		level = lvl if lvl > 0 and lvl < maxlvl else maxlvl
-		self.base_atk = lvl1atk + (maxatk-lvl1atk) * (level-1) / (maxlvl-1)
-		self.pot = pot if pot in range(1,7) else 1
-		if self.pot > 3: self.base_atk += 25
-		
-		self.skill = skill if skill in [1,2,3] else 3 ###### check implemented skills
-		self.mastery = mastery if mastery in [0,1,2,3] else 3
-		if level != maxlvl: self.name = f"Angelina Lv{level} P{self.pot} S{self.skill}" #####set op name
-		else: self.name = f"Angelina P{self.pot} S{self.skill}"
-		if self.mastery == 0: self.name += "L7"
-		elif self.mastery < 3: self.name += f"M{self.mastery}"
-		self.targets = max(1,targets)
-
-		self.moduledmg = TrTaTaSkMo[4]
-		
-		self.module = module if module in [0,1,2] else 1 ##### check valid modules
-		self.module_lvl = module_lvl if module_lvl in [1,2,3] else 3		
-		if level >= maxlvl-30:
-			if self.module == 1:
-				if self.module_lvl == 3: self.base_atk += 20
-				elif self.module_lvl == 2: self.base_atk += 15
-				else: self.base_atk += 10
-				self.name += f" ModX{self.module_lvl}"
-			elif self.module == 2:
-				if self.module_lvl == 3: self.base_atk += 50
-				elif self.module_lvl == 2: self.base_atk += 45
-				else: self.base_atk += 35
-				self.name += f" ModY{self.module_lvl}"
-			else: self.name += " no Mod"
-		else: self.module = 0
-		
-		if self.targets > 1 and self.skill == 3: self.name += f" {self.targets}targets" ######when op has aoe
-		
-		self.buffs = buffs
-			
+	def __init__(self, pp, *args, **kwargs):
+		super().__init__("Angelina", pp, [1,2,3], [2,1],3,1,1)
+		if self.targets > 1 and self.skill == 3: self.name += f" {self.targets}targets" ######when op has aoe	
 	
 	def skill_dps(self, defense, res):
-		dps = 0
-		atkbuff = self.buffs[0]
-		aspd = self.buffs[2]
-		atk_scale = 1
-		
-		#talent/module buffs
-		aspd += 8 if self.pot == 6 else 7
+		aspd = self.talent1_params[0]
 		if self.module == 1:
-			aspd += 3 + self.module_lvl
 			if self.module_lvl == 2: aspd += 3
 			if self.module_lvl == 3: aspd += 5
-			
+
 		####the actual skills
 		if self.skill == 1:
-			atkbuff += 0.8 + 0.1 * self.mastery
-			
-			final_atk = self.base_atk * (1+atkbuff) + self.buffs[1]
+			final_atk = self.atk * (1 + self.buff_atk + self.skill_params[0]) + self.buff_atk_flat
 
 			hitdmg = np.fmax(final_atk * (1-res/100), final_atk * 0.05)
-			dps = hitdmg/(self.atk_interval/(1+aspd/100))
+			dps = hitdmg/self.atk_interval * (self.attack_speed + aspd)/ 100
 		if self.skill == 2:
-			self.atk_interval = 0.285
-			skill_scale = 0.45 if self.mastery == 3 else 0.4
-			
-			final_atk = self.base_atk * (1+atkbuff) + self.buffs[1]
+			atk_interval = self.atk_interval * 0.15
+			skill_scale = self.skill_params[1]
+			final_atk = self.atk * (1 + self.buff_atk) + self.buff_atk_flat
 
 			hitdmg = np.fmax(final_atk * skill_scale * (1-res/100), final_atk * skill_scale * 0.05)
-			dps = hitdmg/(self.atk_interval/(1+aspd/100))
+			dps = hitdmg / atk_interval * (self.attack_speed + aspd)/100
 		if self.skill == 3:
-			atkbuff += 1.05 + 0.15 * self.mastery
-			
-			final_atk = self.base_atk * (1+atkbuff) + self.buffs[1]
-
+			targets = self.skill_params[1]
+			final_atk = self.atk * (1 + self.buff_atk + self.skill_params[0]) + self.buff_atk_flat
 			hitdmg = np.fmax(final_atk * (1-res/100), final_atk * 0.05)
-			dps = hitdmg/(self.atk_interval/(1+aspd/100)) * min(self.targets, 5)
+			dps = hitdmg/self.atk_interval * (self.attack_speed + aspd)/ 100 * min(self.targets, targets)
 		return dps
 
 class April(Operator):
