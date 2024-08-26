2eb3fd11f0de9fa000dd9bf7b70c02ddbf711354
WhoAteMyCQQkie <128926564+WhoAteMyCQQkie@users.noreply.github.com>
2024-08-26 19:40:42 +0200

    Updated Estelle

--- damagecalc/damage_formulas.py
+++ damagecalc/damage_formulas.py
@@ -3415,53 +3415,15 @@ class Ela(Operator):
 		return dps
 	
 class Estelle(Operator):
-	def __init__(self, pp, lvl = 0, pot=-1, skill=-1, mastery = 3, module=-1, module_lvl = 3, targets=1, TrTaTaSkMo=[True,True,True,True,True], buffs=[0,0,0],**kwargs):
-		maxlvl=70
-		lvl1atk = 524  #######including trust
-		maxatk = 690
-		self.atk_interval = 1.2   #### in seconds
-		level = lvl if lvl > 0 and lvl < maxlvl else maxlvl
-		self.base_atk = lvl1atk + (maxatk-lvl1atk) * (level-1) / (maxlvl-1)
-		self.pot = pot if pot in range(1,7) else 6
-		
-		self.skill = skill if skill in [1,2] else 2 ###### check implemented skills
-		self.mastery = mastery if mastery in [0,1,2,3] else 3
-		if level != maxlvl: self.name = f"Estelle Lv{level} P{self.pot} S{self.skill}" #####set op name
-		else: self.name = f"Estelle P{self.pot} S{self.skill}"
-		if self.mastery == 0: self.name += "L7"
-		elif self.mastery < 3: self.name += f"M{self.mastery}"
-		self.targets = max(1,targets)
-
-		
-		self.module = module if module in [0,2] else 2 ##### check valid modules
-		self.module_lvl = module_lvl if module_lvl in [1,2,3] else 3		
-		if level >= maxlvl-30:
-			if self.module == 2:
-				if self.module_lvl == 3: self.base_atk += 52
-				elif self.module_lvl == 2: self.base_atk += 43
-				else: self.base_atk += 32
-				self.name += f" ModY{self.module_lvl}"
-			else: self.name += " no Mod"
-		else: self.module = 0
-		
-		
+	def __init__(self, pp, *args, **kwargs):
+		super().__init__("Estelle",pp,[1,2],[2],2,6,2)
 		if self.targets > 1: self.name += f" {self.targets}targets" ######when op has aoe
-		
-		self.buffs = buffs
-			
-	
-	def skill_dps(self, defense, res):
-		dps = 0
-		atkbuff = self.buffs[0]
-		aspd = self.buffs[2]
-			
-		####the actual skills
-		if self.skill == 1: atkbuff += 0.5 + 0.1 * self.mastery
-		else: atkbuff += 1.5 if self.mastery == 3 else 1.15 + 0.1 * self.mastery	
 
-		final_atk = self.base_atk * (1+atkbuff) + self.buffs[1]	
+	def skill_dps(self, defense, res):
+		final_atk = self.atk * (1 + self.buff_atk + self.skill_params[0]) + self.buff_atk_flat
 		hitdmg = np.fmax(final_atk - defense, final_atk * 0.05)
-		dps = hitdmg/(self.atk_interval/(1+aspd/100)) * min(self.targets,3)
+		block = 3 if self.elite == 2 else 2
+		dps = hitdmg/self.atk_interval * self.attack_speed/100 * min(self.targets,block)
 		return dps
 
 class Eunectes(Operator):
@@ -3661,44 +3623,26 @@ class ExecutorAlter(Operator):
 class Exusiai(Operator):
 	def __init__(self, pp, *args, **kwargs):
 		super().__init__("Exusiai",pp,[1,2,3],[],3,1,1)
-		
-		if self.module_dmg and self.module == 1: self.name += " aerial target"
-				
+		if self.module_dmg and self.module == 1: self.name += " aerial target"	
 	
 	def skill_dps(self, defense, res):
-		dps = 0
 		atkbuff = min(self.talent2_params) #they changed the order in the module ffs
 		aspd = self.talent1_params[0]
-		atk_scale = 1
-
-		if self.module == 1 and self.module_dmg:
-			atk_scale = 1.1
-			
-		####the actual skills
+		atk_scale = 1.1 if self.module == 1 and self.module_dmg else 1
+		final_atk = self.atk * (1+atkbuff+self.buff_atk) + self.buff_atk_flat
+		skill_scale = self.skill_params[0]
 		if self.skill == 1:
-			skill_scale = self.skill_params[0]
-			
-			final_atk = self.atk * (1+atkbuff+self.buff_atk) + self.buff_atk_flat
-			
 			hitdmg = np.fmax(final_atk * atk_scale - defense, final_atk * atk_scale * 0.05)
-			skillhitdmg = np.fmax(final_atk * atk_scale *skill_scale - defense, final_atk* atk_scale * skill_scale * 0.05)
-			sp_cost = self.skill_cost
-			avgphys = (sp_cost * hitdmg + 3 * skillhitdmg) / (sp_cost + 1)
+			skillhitdmg = np.fmax(final_atk * atk_scale * skill_scale - defense, final_atk* atk_scale * skill_scale * 0.05)
+			avgphys = (self.skill_cost * hitdmg + 3 * skillhitdmg) / (self.skill_cost + 1)
 			dps = avgphys/(self.atk_interval/((self.attack_speed+aspd)/100))
-			
 		elif self.skill == 2:
-			skill_scale = self.skill_params[0]
-			final_atk = self.atk * (1+atkbuff+self.buff_atk) + self.buff_atk_flat
 			hitdmg = np.fmax(final_atk *atk_scale * skill_scale - defense, final_atk* atk_scale* skill_scale * 0.05)
 			dps = 4*hitdmg/(self.atk_interval/((self.attack_speed+aspd)/100))
-			
-		else:
-			skill_scale = self.skill_params[0]
+		elif self.skill == 3:
 			atk_interval = self.atk_interval + 2 * self.skill_params[2]
-			final_atk = self.atk * (1+atkbuff+self.buff_atk) + self.buff_atk_flat
-			hitdmg = np.fmax(final_atk *atk_scale * skill_scale - defense, final_atk* atk_scale* skill_scale * 0.05)
+			hitdmg = np.fmax(final_atk * atk_scale * skill_scale - defense, final_atk* atk_scale* skill_scale * 0.05)
 			dps = 5*hitdmg/(atk_interval/((self.attack_speed+aspd)/100))
-		
 		return dps
 		
 class Eyjafjalla(Operator):
