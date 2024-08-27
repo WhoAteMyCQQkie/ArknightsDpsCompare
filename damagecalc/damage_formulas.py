f551d48ba50555fde831474e8d8c26d6d8a91a4e
WhoAteMyCQQkie <128926564+WhoAteMyCQQkie@users.noreply.github.com>
2024-08-27 20:47:55 +0200

    Updated kyaa Skadi

--- damagecalc/damage_formulas.py
+++ damagecalc/damage_formulas.py
@@ -10420,88 +10420,20 @@ class SilverAsh(Operator):
 		return dps
 	
 class Skadi(Operator):
-	def __init__(self, pp, lvl = 0, pot=-1, skill=-1, mastery = 3, module=-1, module_lvl = 3, targets=1, TrTaTaSkMo=[True,True,True,True,True], buffs=[0,0,0],**kwargs):
-		maxlvl=90
-		lvl1atk = 922  #######including trust
-		maxatk = 1095
-		self.atk_interval = 1.5   #### in seconds
-		level = lvl if lvl > 0 and lvl < maxlvl else maxlvl
-		self.base_atk = lvl1atk + (maxatk-lvl1atk) * (level-1) / (maxlvl-1)
-		self.pot = pot if pot in range(1,7) else 1
-		if self.pot > 3: self.base_atk += 33
-		
-		self.skill = skill if skill in [1,2,3] else 2 ###### check implemented skills
-		self.mastery = mastery if mastery in [0,1,2,3] else 3
-		if level != maxlvl: self.name = f"Skadi Lv{level} P{self.pot} S{self.skill}" #####set op name
-		else: self.name = f"Skadi P{self.pot} S{self.skill}"
-		if self.mastery == 0: self.name += "L7"
-		elif self.mastery < 3: self.name += f"M{self.mastery}"
-		self.targets = max(1,targets)
-		self.trait = TrTaTaSkMo[0]
-		self.talent1 = TrTaTaSkMo[1]
-		self.talent2 = TrTaTaSkMo[2]
-		self.skilldmg = TrTaTaSkMo[3]
-		self.moduledmg = TrTaTaSkMo[4]
-		
-		self.module = module if module in [0,1,2] else 1 ##### check valid modules
-		self.module_lvl = module_lvl if module_lvl in [1,2,3] else 3		
-		if level >= maxlvl-30:
-			if self.module == 1:
-				if self.module_lvl == 3: self.base_atk += 105
-				elif self.module_lvl == 2: self.base_atk += 90
-				else: self.base_atk += 70
-				self.name += f" ModX{self.module_lvl}"
-			elif self.module == 2:
-				if self.module_lvl == 3: self.base_atk += 85
-				elif self.module_lvl == 2: self.base_atk += 75
-				else: self.base_atk += 55
-				self.name += f" ModY{self.module_lvl}"
-			else: self.name += " no Mod"
-		else: self.module = 0
-		
-		if self.moduledmg: 
+	def __init__(self, pp, *args, **kwargs):
+		super().__init__("Skadi",pp,[1,2,3],[2,1],2,1,1)
+		if self.module_dmg: 
 			if self.module == 1: self.name += " vsBlocked"
-			if self.module == 2: self.name += " afterRevive"
-		
-		if self.targets > 1: self.name += f" {self.targets}targets" ######when op has aoe
-		
-		self.buffs = buffs
-			
+			if self.module == 2: self.name += " afterRevive"	
 	
 	def skill_dps(self, defense, res):
-		dps = 0
-		atkbuff = self.buffs[0]
-		aspd = self.buffs[2]
-		atk_scale = 1
-		
-		atkbuff += 0.14
-		if self.pot > 4: atkbuff += 0.02
-		if self.module == 2:
-			if self.moduledmg: aspd += 30
-			atkbuff += 0.04 * (self.module_lvl - 1)
-		if self.module == 1 and self.moduledmg: atk_scale = 1.15
-			
-		if self.skill == 1:
-			atkbuff += 0.34 + 0.03 * self.mastery
-			aspd += 35
-			if self.mastery == 3:
-				atkbuff += 0.02
-				aspd += 10
-			final_atk = self.base_atk * (1+atkbuff) + self.buffs[1]
-			hitdmg = np.fmax(final_atk *atk_scale - defense, final_atk* atk_scale * 0.05)
-			dps = hitdmg/(self.atk_interval/(1+aspd/100))
-		if self.skill == 2:
-			atkbuff += 1.2 + 0.15 * self.mastery
-			if self.mastery == 3: atkbuff += 0.05
-			final_atk = self.base_atk * (1+atkbuff) + self.buffs[1]
-			hitdmg = np.fmax(final_atk *atk_scale - defense, final_atk* atk_scale * 0.05)
-			dps = hitdmg/(self.atk_interval/(1+aspd/100))
-		if self.skill == 3:
-			atkbuff += 1 + 0.1 * self.mastery
-			final_atk = self.base_atk * (1+atkbuff) + self.buffs[1]
-			hitdmg = np.fmax(final_atk *atk_scale - defense, final_atk* atk_scale * 0.05)
-			dps = hitdmg/(self.atk_interval/(1+aspd/100))
-			
+		atkbuff = self.talent1_params[0] + self.skill_params[0]
+		aspd = 0 if self.skill != 1 else self.skill_params[1]
+		atk_scale = 1.15 if self.module == 1 and self.module_dmg else 1
+		if self.module == 2 and self.module_dmg: aspd += 30
+		final_atk = self.atk * (1 + atkbuff + self.buff_atk) + self.buff_atk_flat
+		hitdmg = np.fmax(final_atk *atk_scale - defense, final_atk* atk_scale * 0.05)
+		dps = hitdmg/(self.atk_interval/((self.attack_speed+aspd)/100))
 		return dps
 
 class Skalter(Operator):
