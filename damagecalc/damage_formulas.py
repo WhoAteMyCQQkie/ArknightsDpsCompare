affb2845729ceec46194dff1eeef12b6f683688c
WhoAteMyCQQkie <128926564+WhoAteMyCQQkie@users.noreply.github.com>
2024-08-27 10:39:10 +0200

    Updated Chalter (still needs S2)

--- damagecalc/damage_formulas.py
+++ damagecalc/damage_formulas.py
@@ -2342,37 +2342,8 @@ class Chen(Operator):
 
 class ChenAlter(Operator):
 	def __init__(self, pp, lvl = 0, pot=-1, skill=-1, mastery = 3, module=-1, module_lvl = 3, targets=1, TrTaTaSkMo=[True,True,True,True,True], buffs=[0,0,0],**kwargs):
-		maxlvl=90
-		lvl1atk = 729  #######including trust
-		maxatk = 853
-		self.atk_interval = 2.3   #### in seconds
-		level = lvl if lvl > 0 and lvl < maxlvl else maxlvl
-		self.base_atk = lvl1atk + (maxatk-lvl1atk) * (level-1) / (maxlvl-1)
-		self.pot = pot if pot in range(1,7) else 1
-		if self.pot > 3: self.base_atk += 33
-		
-		self.skill = skill if skill in [3] else 3 ###### check implemented skills
-		self.mastery = mastery if mastery in [0,1,2,3] else 3
-		if level != maxlvl: self.name = f"Chalter Lv{level} P{self.pot} S{self.skill}" #####set op name
-		else: self.name = f"Chalter P{self.pot} S{self.skill}"
-		if self.mastery == 0: self.name += "L7"
-		elif self.mastery < 3: self.name += f"M{self.mastery}"
-		self.targets = max(1,targets)
-		
-		self.module = module if module in [0,1] else 1 ##### check valid modules
-		self.module_lvl = module_lvl if module_lvl in [1,2,3] else 3		
-		if level >= maxlvl-30:
-			if self.module == 1:
-				if self.module_lvl == 3: self.base_atk += 60
-				elif self.module_lvl == 2: self.base_atk += 45
-				else: self.base_atk += 30
-				self.name += f" ModX{self.module_lvl}"
-			else: self.name += " no Mod"
-		else: self.module = 0
-		
+		super().__init__("ChenAlter",pp,[1,3],[1],3,1,1)
 		if self.targets > 1: self.name += f" {self.targets}targets" ######when op has aoe
-		
-		self.buffs = buffs
 		try:
 			self.shreds = kwargs['shreds']
 		except KeyError:
@@ -2381,16 +2352,17 @@ class ChenAlter(Operator):
 	
 	def skill_dps(self, defense, res):
 		dps = 0
-		atkbuff = self.buffs[0]
-		aspd = self.buffs[2] + 8 #im not going to include the water buff for now
+		atkbuff = self.skill_params[0]
+		aspd = 8 if self.elite == 2 else 0 #im not going to include the water buff for now
 		atk_scale = 1.6 if self.module == 1 else 1.5
-				
-			
+		final_atk = self.atk * (1 + atkbuff + self.buff_atk) + self.buff_atk_flat
+		if self.skill == 1:
+			hitdmg = np.fmax(final_atk * atk_scale - defense, final_atk* atk_scale * 0.05)
+			dps = hitdmg/self.atk_interval * (self.attack_speed+aspd)/100 * self.targets
+
 		####the actual skills
 		if self.skill == 3:
-			atkbuff += 0.7 + 0.1 * self.mastery 
-			def_shred = 200 if self.mastery == 0 else 220
-			final_atk = self.base_atk * (1+atkbuff) + self.buffs[1]
+			def_shred = self.skill_params[2] * (-1)
 			if self.shreds[0] < 1 and self.shreds[0] > 0:
 				defense = defense / self.shreds[0]
 			newdefense = np.fmax(0, defense- def_shred)
@@ -2398,7 +2370,7 @@ class ChenAlter(Operator):
 				newdefense *= self.shreds[0]
 			hitdmg = np.fmax(final_atk * atk_scale - newdefense, final_atk* atk_scale * 0.05)
 			
-			dps = 2*hitdmg/(self.atk_interval/(1+aspd/100)) * self.targets
+			dps = 2 * hitdmg/self.atk_interval * (self.attack_speed+aspd)/100 * self.targets
 		return dps
 	
 	def total_dmg(self, defense, res):
