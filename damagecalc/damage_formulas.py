48a648adc7eb096a85efc30641d18dc32c95e008
WhoAteMyCQQkie <128926564+WhoAteMyCQQkie@users.noreply.github.com>
2024-08-27 21:36:57 +0200

    Updated Ulpianus

--- damagecalc/damage_formulas.py
+++ damagecalc/damage_formulas.py
@@ -11307,74 +11307,49 @@ class Typhon(Operator):
 
 class Ulpianus(Operator):
 	def __init__(self, pp, lvl = 0, pot=-1, skill=-1, mastery = 3, module=-1, module_lvl = 3, targets=1, TrTaTaSkMo=[True,True,True,True,True], buffs=[0,0,0],**kwargs):
-		maxlvl=90
-		lvl1atk = 1397  #######including trust
-		maxatk = 1649
-		self.atk_interval = 2.5   #### in seconds
-		level = lvl if lvl > 0 and lvl < maxlvl else maxlvl
-		self.base_atk = lvl1atk + (maxatk-lvl1atk) * (level-1) / (maxlvl-1)
-		self.pot = pot if pot in range(1,7) else 1
-		if self.pot > 3: self.base_atk += 45
-		
-		self.skill = skill if skill in [2,3] else 3 ###### check implemented skills
-		self.mastery = mastery if mastery in [0,1,2,3] else 3
-		if level != maxlvl: self.name = f"Ulpianus Lv{level} P{self.pot} S{self.skill}" #####set op name
-		else: self.name = f"Ulpianus P{self.pot} S{self.skill}"
-		if self.mastery == 0: self.name += "L7"
-		elif self.mastery < 3: self.name += f"M{self.mastery}"
-		self.targets = max(1,targets)
-		self.trait = TrTaTaSkMo[0]
-		self.talent1 = TrTaTaSkMo[1]
-		self.talent2 = TrTaTaSkMo[2]
-		self.skilldmg = TrTaTaSkMo[3]
-		self.moduledmg = TrTaTaSkMo[4]
-		
-		maxkills = 10 if self.pot > 4 else 9
-		if self.talent2: self.name += f" {maxkills}kills"
-		
+		super().__init__("Ulpianus",pp,[1,2,3],[],3,1,0)
+		if self.elite == 2 and self.talent2_dmg:
+			self.name += f" {int(self.talent2_params[0])}kills"
 		if self.targets > 1: self.name += f" {self.targets}targets" ######when op has aoe
-		
-		self.buffs = buffs
+
+		if self.skill == 3:
+			bonus_base = self.talent2_params[0] * self.talent2_params[2] if self.talent2_dmg and self.elite == 2 else 0
+			atkbuff = self.skill_params[1]
+			final_atk = (self.atk + bonus_base) * (1 + atkbuff + self.buff_atk) + self.buff_atk_flat
+			scale = self.skill_params[2]
+			nukedmg = final_atk * scale * (1+self.buff_fragile)
+			self.name += f" InitialDmg:{int(nukedmg)}"
+			
 	
 	def skill_dps(self, defense, res):
-		dps = 0
-		atkbuff = self.buffs[0]
-		aspd = self.buffs[2]
-		atk_scale = 1
-		flatbuff = 0
-		#talent/module buffs
-		
-		bonusbase_atk = 0
-		if self.talent2:
-			bonusbase_atk = 300 if self.pot > 4 else 270
+		bonus_base = self.talent2_params[0] * self.talent2_params[2] if self.talent2_dmg and self.elite == 2 else 0
 			
 		####the actual skills
 		if self.skill == 1:
-			skill_scale = 2.1 + 0.2 * self.mastery
-			sp_cost = 4 if self.mastery == 3 else 5
-			final_atk = (self.base_atk + bonusbase_atk) * (1+atkbuff) + self.buffs[1]	+ flatbuff
+			skill_scale = self.skill_params[0]
+			sp_cost = self.skill_cost
+			final_atk = (self.atk + bonus_base) * (1 + self.buff_atk) + self.buff_atk_flat
+			hitdmg = np.fmax(final_atk - defense, final_atk * 0.05)
+			skilldmg = np.fmax(final_atk * skill_scale - defense, final_atk * skill_scale * 0.05)
+			sp_cost = sp_cost/(1+self.sp_boost) + 1.2 #sp lockout
+			atkcycle = self.atk_interval/(self.attack_speed/100)
+			atks_per_skillactivation = sp_cost / atkcycle
+			avghit = skilldmg
+			if atks_per_skillactivation > 1:
+				avghit = (skilldmg + int(atks_per_skillactivation) * hitdmg) / (int(atks_per_skillactivation)+1)
+				
+			dps = avghit/self.atk_interval * self.attack_speed/100 * min(2,self.targets)
 		if self.skill == 2:
-			atkbuff += 1.3 + 0.1 * self.mastery
-			final_atk = (self.base_atk + bonusbase_atk) * (1+atkbuff) + self.buffs[1]	+ flatbuff
+			atkbuff = self.skill_params[1]
+			final_atk = (self.atk + bonus_base) * (1 + atkbuff + self.buff_atk) + self.buff_atk_flat
 			hitdmg = np.fmax(final_atk - defense, final_atk * 0.05)
-			dps = hitdmg/(self.atk_interval/(1+aspd/100)) * min(self.targets,3)
+			dps = hitdmg/self.atk_interval * self.attack_speed/100 * min(3,self.targets)
 		if self.skill == 3:
-			atkbuff += 2.3 + 0.1 * self.mastery
-			final_atk = (self.base_atk + bonusbase_atk) * (1+atkbuff) + self.buffs[1]	+ flatbuff
+			atkbuff = self.skill_params[1]
+			final_atk = (self.atk + bonus_base) * (1 + atkbuff + self.buff_atk) + self.buff_atk_flat
 			hitdmg = np.fmax(final_atk - defense, final_atk * 0.05)
-			dps = hitdmg/(self.atk_interval/(1+aspd/100)) * min(self.targets,2)
+			dps = hitdmg/self.atk_interval * self.attack_speed/100 * min(2,self.targets)
 		return dps
-
-	def get_name(self):
-		if self.skill == 3:
-			bonusbase_atk = 0
-			if self.talent2:
-				bonusbase_atk = 300 if self.pot > 4 else 270
-			final_atk = (self.base_atk + bonusbase_atk) * (1+self.buffs[0] + 2.3 + 0.1 * self.mastery) + self.buffs[1]
-			scale = 1.6 if self.mastery == 3 else 1.4 + 0.05 * self.mastery
-			nukedmg = final_atk * scale * (1+self.buffs[3])
-			self.name += f" InitialDmg:{int(nukedmg)}"
-		return self.name
 
 class Utage(Operator):
 	def __init__(self, pp, lvl = 0, pot=-1, skill=-1, mastery = 3, module=-1, module_lvl = 3, targets=1, TrTaTaSkMo=[True,True,True,True,True], buffs=[0,0,0],**kwargs):
