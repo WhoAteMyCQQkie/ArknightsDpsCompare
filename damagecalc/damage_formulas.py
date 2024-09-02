e343dd56d65e5f2e42fa167e5f1aea06b6c87438
WhoAteMyCQQkie <128926564+WhoAteMyCQQkie@users.noreply.github.com>
2024-09-02 16:35:55 +0200

    Updated Quartz

--- damagecalc/damage_formulas.py
+++ damagecalc/damage_formulas.py
@@ -9566,49 +9566,22 @@ class Qiubai(Operator):
 	
 class Quartz(Operator):
 	def __init__(self, pp, lvl = 0, pot=-1, skill=-1, mastery = 3, module=-1, module_lvl = 3, targets=1, TrTaTaSkMo=[True,True,True,True,True], buffs=[0,0,0],**kwargs):
-		maxlvl=70
-		lvl1atk = 1201  #######including trust
-		maxatk = 1437
-		self.atk_interval = 2.5   #### in seconds
-		level = lvl if lvl > 0 and lvl < maxlvl else maxlvl
-		self.base_atk = lvl1atk + (maxatk-lvl1atk) * (level-1) / (maxlvl-1)
-		self.pot = pot if pot in range(1,7) else 6
-		if self.pot > 3: self.base_atk += 35
-		
-		self.skill = skill if skill in [1,2] else 2 ###### check implemented skills
-		self.mastery = mastery if mastery in [0,1,2,3] else 3
-		if level != maxlvl: self.name = f"Quartz Lv{level} P{self.pot} S{self.skill}" #####set op name
-		else: self.name = f"Quartz P{self.pot} S{self.skill}"
-		if self.mastery == 0: self.name += "L7"
-		elif self.mastery < 3: self.name += f"M{self.mastery}"
-		self.targets = max(1,targets)
-
+		super().__init__("Quartz",pp,[1,2],[1],1,6,1)
 		if self.targets > 1: self.name += f" {self.targets}targets" ######when op has aoe
-		
-		self.buffs = buffs
 	
 	def skill_dps(self, defense, res):
-		dps = 0
-		atkbuff = self.buffs[0]
-		aspd = self.buffs[2]
-		atk_scale = 1
-		
-		#talent/module buffs
-		atkbuff += 0.09 if self.pot > 4 else 0.08
-			
-		####the actual skills
+		atkbuff = self.talent1_params[1]
 		if self.skill == 1:
-			atkbuff += 0.5 + 0.1 * self.mastery
-			final_atk = self.base_atk * (1+atkbuff) + self.buffs[1]
+			atkbuff += self.skill_params[0]
+			final_atk = self.atk * (1 + self.buff_atk + atkbuff) + self.buff_atk_flat
 			hitdmg = np.fmax(final_atk - defense, final_atk * 0.05)
-			dps = hitdmg/(self.atk_interval/(1+aspd/100))
+			dps = hitdmg/self.atk_interval * self.attack_speed/100
 		if self.skill == 2:
-			aspd += 65 + 5 * self.mastery
-			skill_scale = 1.2 if self.mastery == 3 else 1
-			
-			final_atk = self.base_atk * (1+atkbuff) + self.buffs[1]
+			aspd = self.skill_params[1]
+			skill_scale = self.skill_params[2]
+			final_atk = self.atk * (1 + self.buff_atk + atkbuff) + self.buff_atk_flat
 			hitdmg = np.fmax(final_atk * skill_scale - defense, final_atk * skill_scale * 0.05)
-			dps = hitdmg/(self.atk_interval/(1+aspd/100))
+			dps = hitdmg/self.atk_interval * (self.attack_speed+aspd)/100
 		return dps * min(self.targets,2)
 
 class Ray(Operator):
