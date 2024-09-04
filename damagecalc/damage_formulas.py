dac503ef864b83307b42a270bae14710272fad61
WhoAteMyCQQkie <128926564+WhoAteMyCQQkie@users.noreply.github.com>
2024-09-04 17:24:56 +0200

    Updated Lin

--- damagecalc/damage_formulas.py
+++ damagecalc/damage_formulas.py
@@ -6718,62 +6718,22 @@ class Leto(Operator):
 		return dps
 
 class Lin(Operator):
-	def __init__(self, pp, lvl = 0, pot=-1, skill=-1, mastery = 3, module=-1, module_lvl = 3, targets=1, TrTaTaSkMo=[True,True,True,True,True], buffs=[0,0,0],**kwargs):
-		maxlvl=90
-		lvl1atk = 800  #######including trust
-		maxatk = 919
-		self.atk_interval = 2.0   #### in seconds
-		level = lvl if lvl > 0 and lvl < maxlvl else maxlvl
-		self.base_atk = lvl1atk + (maxatk-lvl1atk) * (level-1) / (maxlvl-1)
-		self.pot = pot if pot in range(1,7) else 1
-		if self.pot > 3: self.base_atk += 34
-		
-		self.skill = skill if skill in [1,2,3] else 3 ###### check implemented skills
-		self.mastery = mastery if mastery in [0,1,2,3] else 3
-		if level != maxlvl: self.name = f"Lin Lv{level} P{self.pot} S{self.skill}" #####set op name
-		else: self.name = f"Lin P{self.pot} S{self.skill}"
-		if self.mastery == 0: self.name += "L7"
-		elif self.mastery < 3: self.name += f"M{self.mastery}"
-		self.targets = max(1,targets)
-
-		self.module = module if module in [0,1] else 1 ##### check valid modules
-		self.module_lvl = module_lvl if module_lvl in [1,2,3] else 3		
-		if level >= maxlvl-30:
-			if self.module == 1:
-				if self.module_lvl == 3: self.base_atk += 77
-				elif self.module_lvl == 2: self.base_atk += 70
-				else: self.base_atk += 62
-				self.name += f" ModX{self.module_lvl}"
-			else: self.name += " no Mod"
-		else: self.module = 0
-		
-		if self.targets > 1: self.name += f" {self.targets}targets" ######when op has aoe
-		
-		self.buffs = buffs
+	def __init__(self, pp, *args, **kwargs):
+		super().__init__("Lin",pp,[1,2,3],[1],3,1,1)
+		if self.targets > 1: self.name += f" {self.targets}targets"
 	
 	def skill_dps(self, defense, res):
-		dps = 0
-		atkbuff = self.buffs[0]
-		aspd = self.buffs[2]
-
-		if self.skill == 1:
-			self.atk_interval = 3
-			atkbuff += 0.6 if self.mastery == 3 else 0.4 + 0.05 * self.mastery
-			
-			final_atk = self.base_atk * (1+atkbuff) + self.buffs[1]
-			hitdmgarts = np.fmax(final_atk * (1-res/100), final_atk * 0.05)
-			dps = hitdmgarts/(self.atk_interval/(1+aspd/100)) * self.targets
 		if self.skill == 2:
-			aspd += 130 if self.mastery == 3 else 90 + 10 * self.mastery
-			final_atk = self.base_atk * (1+atkbuff) + self.buffs[1]
+			aspd = self.skill_params[0]
+			final_atk = self.atk * (1 + self.buff_atk) + self.buff_atk_flat
 			hitdmgarts = np.fmax(final_atk * (1-res/100), final_atk * 0.05)
-			dps = hitdmgarts/(self.atk_interval/(1+aspd/100)) * self.targets
-		if self.skill == 3:
-			atkbuff += 1.6 + 0.15 * self.mastery
-			if self.mastery > 1: atkbuff -= 0.05
-			final_atk = self.base_atk * (1+atkbuff) + self.buffs[1]
+			dps = hitdmgarts/self.atk_interval * (self.attack_speed+aspd)/100 * self.targets
+		else:
+			if self.skill == 1: self.atk_interval = 3
+			atkbuff = self.skill_params[0]
+			final_atk = self.atk * (1 + atkbuff + self.buff_atk) + self.buff_atk_flat
 			hitdmgarts = np.fmax(final_atk * (1-res/100), final_atk * 0.05)
-			dps = hitdmgarts/(self.atk_interval/(1+aspd/100)) * self.targets
+			dps = hitdmgarts/self.atk_interval * self.attack_speed/100 * self.targets
 		return dps
 
 class Ling(Operator):
