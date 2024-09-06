4609c62f875a553efc2f557fe43e97b8014d55cf
WhoAteMyCQQkie <128926564+WhoAteMyCQQkie@users.noreply.github.com>
2024-09-06 12:02:55 +0200

    Updated Fuze

--- damagecalc/damage_formulas.py
+++ damagecalc/damage_formulas.py
@@ -4170,57 +4170,20 @@ class Franka(Operator):
 		return dps
 	
 class Fuze(Operator):
-	def __init__(self, pp, lvl = 0, pot=-1, skill=-1, mastery = 3, module=-1, module_lvl = 3, targets=1, TrTaTaSkMo=[True,True,True,True,True], buffs=[0,0,0],**kwargs):
-		maxlvl=80
-		lvl1atk = 644  #######including trust
-		maxatk = 835
-		self.atk_interval = 1.2   #### in seconds
-		level = lvl if lvl > 0 and lvl < maxlvl else maxlvl
-		self.base_atk = lvl1atk + (maxatk-lvl1atk) * (level-1) / (maxlvl-1)
-		self.pot = pot if pot in range(1,7) else 6
-		
-		self.skill = skill if skill in [1] else 1 ###### check implemented skills
-		self.mastery = mastery if mastery in [0,1,2,3] else 3
-		if level != maxlvl: self.name = f"Fuze Lv{level} P{self.pot} S{self.skill}" #####set op name
-		else: self.name = f"Fuze P{self.pot} S{self.skill}"
-		if self.mastery == 0: self.name += "L7"
-		elif self.mastery < 3: self.name += f"M{self.mastery}"
-		self.targets = max(1,targets)
+	def __init__(self, pp, *args, **kwargs):
+		super().__init__("Fuze",pp,[1,2],[2],1,6,2)
+		if self.targets > 1: self.name += f" {self.targets}targets"
+		if self.skill == 2:
+			explosion_dmg =  (self.atk * (1 + self.buff_atk) + self.buff_atk_flat) * self.skill_params[0] * (1 + self.buff_fragile)
+			self.name += f" Explosion:5x{int(explosion_dmg)}"
 
-		
-		self.module = module if module in [0,2] else 2 ##### check valid modules
-		self.module_lvl = module_lvl if module_lvl in [1,2,3] else 3		
-		if level >= maxlvl-30:
-			if self.module == 2:
-				if self.module_lvl == 3: self.base_atk += 70
-				elif self.module_lvl == 2: self.base_atk += 60
-				else: self.base_atk += 50
-				self.name += f" ModY{self.module_lvl}"
-			else: self.name += " no Mod"
-		else: self.module = 0
-		
-		
-		if self.targets > 1: self.name += f" {self.targets}targets" ######when op has aoe
-		
-		self.buffs = buffs
-		if self.buffs[0]>0: self.name += f" atk+{int(100*self.buffs[0])}%"
-		if self.buffs[1]>0: self.name += f" atk+{self.buffs[1]}"
-		if self.buffs[2]>0: self.name += f" aspd+{self.buffs[2]}"
-			
-	
 	def skill_dps(self, defense, res):
-		dps = 0
-		atkbuff = self.buffs[0]
-		aspd = self.buffs[2]
-			
-		####the actual skills
-		aspd += 75 + 5 * self.mastery
-		atkbuff += 0.15 + 0.05 * self.mastery
-		if self.mastery < 2: atkbuff += 0.02
-
-		final_atk = self.base_atk * (1+atkbuff) + self.buffs[1]	
+		atkbuff = self.skill_params[1] if self.skill == 1 else 0
+		aspd = self.skill_params[2] if self.skill == 1 else 0
+		final_atk = self.atk * (1 + atkbuff + self.buff_atk) + self.buff_atk_flat	
 		hitdmg = np.fmax(final_atk - defense, final_atk * 0.05)
-		dps = hitdmg/(self.atk_interval/(1+aspd/100)) * min(self.targets,3)
+		max_targets = 3 if self.elite == 2 else 2
+		dps = hitdmg/self.atk_interval * (self.attack_speed+aspd)/100 * min(self.targets,max_targets)
 		return dps
 
 class GavialAlter(Operator):
