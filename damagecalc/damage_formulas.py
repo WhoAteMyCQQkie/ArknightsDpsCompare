6a3a6aa9fe66fc1536017c5f9601d36c54a551fb
WhoAteMyCQQkie <128926564+WhoAteMyCQQkie@users.noreply.github.com>
2024-09-08 16:02:12 +0200

    Updated Ines

--- damagecalc/damage_formulas.py
+++ damagecalc/damage_formulas.py
@@ -5355,79 +5355,40 @@ class Indra(Operator):
 		return dps
 
 class Ines(Operator):
-	def __init__(self, pp, lvl = 0, pot=-1, skill=-1, mastery = 3, module=-1, module_lvl = 3, targets=1, TrTaTaSkMo=[True,True,True,True,True], buffs=[0,0,0],**kwargs):
-		maxlvl=90
-		lvl1atk = 532  #######including trust
-		maxatk = 639
-		self.atk_interval = 1.0   #### in seconds
-		level = lvl if lvl > 0 and lvl < maxlvl else maxlvl
-		self.base_atk = lvl1atk + (maxatk-lvl1atk) * (level-1) / (maxlvl-1)
-		self.pot = pot if pot in range(1,7) else 1
-		
-		
-		self.skill = skill if skill in [1,2,3] else 2 ###### check implemented skills
-		self.mastery = mastery if mastery in [0,1,2,3] else 3
-		if level != maxlvl: self.name = f"Ines Lv{level} P{self.pot} S{self.skill}" #####set op name
-		else: self.name = f"Ines P{self.pot} S{self.skill}"
-		if self.mastery == 0: self.name += "L7"
-		elif self.mastery < 3: self.name += f"M{self.mastery}"
-		self.targets = max(1,targets)
-		self.trait = TrTaTaSkMo[0]
-		self.talent1 = TrTaTaSkMo[1]
-		self.talent2 = TrTaTaSkMo[2]
-		self.skilldmg = TrTaTaSkMo[3]
-		
-		
+	def __init__(self, pp, *args, **kwargs):
+		super().__init__("Ines",pp,[1,2,3],[],2,1,0)
 		if self.skill == 2:
-			if self.skilldmg: self.name += " maxSteal"
+			if self.skill_dmg: self.name += " maxSteal"
 			else: self.name += " noSteal"
-		
-		if self.targets > 1: self.name += f" {self.targets}targets" ######when op has aoe
-		
-		self.buffs = buffs
-			
-	
-	def skill_dps(self, defense, res):
-		dps = 0
-		atkbuff = self.buffs[0]
-		aspd = self.buffs[2]
-		if self.pot > 3: aspd += 8
-		atk_scale = 1
-		
-		stolenatk = 90 if self.pot < 5 else 100
-			
-		####the actual skills
-		if self.skill == 1:
-			skill_scale = 0.5 + 0.1 * self.mastery 
-			if self.mastery < 3: skill_scale += 0.5
-			
-			final_atk = self.base_atk * (1+atkbuff) + self.buffs[1] + stolenatk
-			
-			hitdmg = np.fmax(final_atk * atk_scale - defense, final_atk * atk_scale * 0.05)
-			skillhitdmg = np.fmax(final_atk * atk_scale *skill_scale - defense, final_atk* atk_scale * skill_scale * 0.05)
-			sp_cost = 3
-			dps = hitdmg/(self.atk_interval/(1+aspd/100)) + skillhitdmg * min(1, (1+aspd/100)*3/4)
-		if self.skill == 2:
-			atkbuff += 0.8 + 0.1 * self.mastery
-			aspdsteal = 7 if self.mastery == 3 else 6
-			aspd += aspdsteal * 10 if self.skilldmg else aspdsteal
-			final_atk = self.base_atk * (1+atkbuff) + self.buffs[1] + stolenatk	
-			hitdmg = np.fmax(final_atk - defense, final_atk * 0.05)
-			dps = hitdmg/(self.atk_interval/(1+aspd/100))
 		if self.skill == 3:
-			atkbuff += 1.2 if self.mastery == 0 else 1.3 + 0.1 * self.mastery
-			final_atk = self.base_atk * (1+atkbuff) + self.buffs[1] + stolenatk
-			hitdmg = np.fmax(final_atk - defense, final_atk * 0.05)
-			dps = hitdmg/(self.atk_interval/(1+aspd/100))	
-		return dps
-		
-	def get_name(self):
-		if self.skill == 3:
-			skillbuff = 1.2 if self.mastery == 0 else 1.3 + 0.1 * self.mastery
-			final_atk = self.base_atk * (1+self.buffs[0] + skillbuff) + self.buffs[1]
-			nukedmg = final_atk * (1.4 + 0.2 * self.mastery) * (1+self.buffs[3])
+			skillbuff = self.skill_params[1]
+			steal = 0 if self.elite < 1 else self.talent1_params[0]
+			final_atk = self.atk * (1 + self.buff_atk + skillbuff) + self.buff_atk_flat + steal
+			nukedmg = final_atk * self.skill_params[2] * (1+self.buff_fragile)
 			self.name += f" ShadowDmg:{int(nukedmg)}"
-		return self.name
+
+	def skill_dps(self, defense, res):
+		stolen_atk = 0 if self.elite < 1 else self.talent1_params[0]
+
+		if self.skill == 1:
+			skill_scale = self.skill_params[1]
+			final_atk = self.atk * (1 + self.buff_atk) + self.buff_atk_flat + stolen_atk
+			hitdmg = np.fmax(final_atk - defense, final_atk * 0.05)
+			skillhitdmg = np.fmax(final_atk * skill_scale * (1 - res/100), final_atk * skill_scale * 0.05)
+			sp_cost = self.skill_cost
+			dps = hitdmg/self.atk_interval * self.attack_speed/100 + skillhitdmg * min(1, 3 / ((sp_cost+1) * self.atk_interval/self.attack_speed*100)) 
+		if self.skill == 2:
+			atkbuff = self.skill_params[1]
+			aspd = self.skill_params[3] if self.skill_dmg else self.skill_params[2]
+			final_atk = self.atk * (1 + self.buff_atk + atkbuff) + self.buff_atk_flat + stolen_atk
+			hitdmg = np.fmax(final_atk - defense, final_atk * 0.05)
+			dps = hitdmg/self.atk_interval * (self.attack_speed + aspd)/100
+		if self.skill == 3:
+			atkbuff = self.skill_params[1]
+			final_atk = self.atk * (1 + self.buff_atk + atkbuff) + self.buff_atk_flat + stolen_atk
+			hitdmg = np.fmax(final_atk - defense, final_atk * 0.05)
+			dps = hitdmg/self.atk_interval * self.attack_speed/100
+		return dps
 
 class Insider(Operator):
 	def __init__(self, pp, *args, **kwargs):
