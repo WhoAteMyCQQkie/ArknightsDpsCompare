3fcc71bcb23d5ffd80eee84f6856d1be59ffa27d
WhoAteMyCQQkie <128926564+WhoAteMyCQQkie@users.noreply.github.com>
2024-09-09 18:00:53 +0200

    Updated Dobermann

--- damagecalc/damage_formulas.py
+++ damagecalc/damage_formulas.py
@@ -2636,65 +2636,28 @@ class Degenbrecher(Operator):
 		return dps
 
 class Dobermann(Operator):
-	def __init__(self, pp, lvl = 0, pot=-1, skill=-1, mastery = 3, module=-1, module_lvl = 3, targets=1, TrTaTaSkMo=[True,True,True,True,True], buffs=[0,0,0,0,0],**kwargs):
-		maxlvl=70
-		lvl1atk = 535  #######including trust
-		maxatk = 632
-		self.atk_interval = 1.05   #### in seconds
-		level = lvl if lvl > 0 and lvl < maxlvl else maxlvl
-		self.base_atk = lvl1atk + (maxatk-lvl1atk) * (level-1) / (maxlvl-1)
-		self.pot = pot if pot in range(1,7) else 6
-
-		self.skill = skill if skill in [1,2] else 2 ###### check implemented skills
-		self.mastery = mastery if mastery in [0,1,2,3] else 3
-		if level != maxlvl: self.name = f"Dobermann Lv{level} P{self.pot} S{self.skill}" #####set op name
-		else: self.name = f"Dobermann P{self.pot} S{self.skill}"
-		if self.mastery == 0: self.name += "L7"
-		elif self.mastery < 3: self.name += f"M{self.mastery}"
-		self.trait = TrTaTaSkMo[0]
-		self.talent = TrTaTaSkMo[1] and TrTaTaSkMo[2]
-		
-		self.module = module if module in [0,2] else 2 ##### check valid modules
-		self.module_lvl = module_lvl if module_lvl in [1,2,3] else 3		
-		if level >= maxlvl-30:
-			if self.module == 2:
-				if self.module_lvl == 3: self.base_atk += 53
-				elif self.module_lvl == 2: self.base_atk += 45
-				else: self.base_atk += 35
-				self.name += f" ModY{self.module_lvl}"
-			else: self.name += " no Mod"
-		else: self.module = 0
-		
-		if not self.trait: self.name += " blocking"   ##### keep the ones that apply
-		if self.module == 2 and self.module_lvl > 1 and self.talent: self.name += " +3star"
-		self.buffs = buffs
+	def __init__(self, pp, *args, **kwargs):
+		super().__init__("Dobermann",pp,[1,2],[2],2,6,2)
+		if not self.trait_dmg: self.name += " blocking"
+		if self.module == 2 and self.module_lvl > 1 and self.talent_dmg: self.name += " +3star"
 			
 	def skill_dps(self, defense, res):
-		dps = 0
-		atkbuff = self.buffs[0]
-		aspd = self.buffs[2]
-		atk_scale = 1.2 if self.trait else 1
-		
-		#talent/module buffs
-		if self.module == 2:
-			aspd +=5
-			if self.talent and self.module_lvl > 1: aspd += 5 * self.module_lvl
-			
-		####the actual skills
+		aspd = 0
+		if self.module == 2 and self.talent_dmg and self.module_lvl > 1: aspd = 5 * self.module_lvl
+		atk_scale = 1.2 if self.trait_dmg else 1
+
 		if self.skill == 1:
-			skill_scale = 2 + 0.1 * self.mastery			
-			final_atk = self.base_atk * (1+atkbuff) + self.buffs[1]
+			skill_scale = self.skill_params[0]			
+			final_atk = self.atk * (1 + self.buff_atk) + self.buff_atk_flat
 			hitdmg = np.fmax(final_atk * atk_scale - defense, final_atk * atk_scale * 0.05)
 			skillhitdmg = np.fmax(final_atk * atk_scale * skill_scale - defense, final_atk * atk_scale * skill_scale * 0.05)
-			sp_cost = 3 if self.mastery == 3 else 4
+			sp_cost = self.skill_cost
 			avgphys = (sp_cost * hitdmg + skillhitdmg) / (sp_cost + 1)
-			dps = avgphys/(self.atk_interval/(1+aspd/100))
-		
+			dps = avgphys/self.atk_interval * (self.attack_speed+aspd)/100
 		if self.skill == 2:
-			atkbuff += 0.5 + 0.1 * self.mastery
-			final_atk = self.base_atk * (1+atkbuff) + self.buffs[1]
+			final_atk = self.atk * (1 + self.buff_atk + self.skill_params[0]) + self.buff_atk_flat
 			hitdmg = np.fmax(final_atk * atk_scale - defense, final_atk * atk_scale * 0.05)
-			dps = hitdmg/(self.atk_interval/(1+aspd/100))
+			dps = hitdmg/self.atk_interval * (self.attack_speed+aspd)/100
 		return dps
 
 class Doc(Operator):
