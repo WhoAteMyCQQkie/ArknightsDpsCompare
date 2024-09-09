bebf161beb12c9e58c65b0b52d643fafd4e6222c
WhoAteMyCQQkie <128926564+WhoAteMyCQQkie@users.noreply.github.com>
2024-09-09 19:08:39 +0200

    Updated Vulcan

--- damagecalc/damage_formulas.py
+++ damagecalc/damage_formulas.py
@@ -9158,8 +9158,7 @@ class SilverAsh(Operator):
 		super().__init__("SilverAsh",pp ,[1,2,3],[1],3,1,1)
 		if not self.trait_dmg and not self.skill == 3: self.name += " rangedAtk"   ##### keep the ones that apply
 		if self.module == 1 and self.module_dmg and self.talent_dmg: self.name += " vsElite"
-		if self.targets > 1 and self.skill == 3: self.name += f" {self.targets}targets" ######when op has aoe
-			
+		if self.targets > 1 and self.skill == 3: self.name += f" {self.targets}targets" ######when op has aoe	
 	
 	def skill_dps(self, defense, res):
 		atk_scale = 1
@@ -9764,8 +9763,7 @@ class Thorns(Operator):
 			self.hits = 0
 		if self.skill == 2: self.name += f" {round(self.hits,2)}hits/s"
 		if self.skill == 3 and not self.skill_dmg: self.name += " firstActivation"
-		if self.targets > 1 and self.skill == 2: self.name += f" {self.targets}targets" ######when op has aoe
-		
+		if self.targets > 1 and self.skill == 2: self.name += f" {self.targets}targets" ######when op has aoe	
 			
 	def skill_dps(self, defense, res):
 		bonus = 0.1 if self.module == 1 else 0
@@ -10384,52 +10382,17 @@ class Viviana(Operator):
 		return dps
 
 class Vulcan(Operator):
-	def __init__(self, pp, lvl = 0, pot=-1, skill=-1, mastery = 3, module=-1, module_lvl = 3, targets=1, TrTaTaSkMo=[True,True,True,True,True], buffs=[0,0,0],**kwargs):
-		maxlvl=80
-		lvl1atk = 689  #######including trust
-		maxatk = 870
-		self.atk_interval = 1.6   #### in seconds
-		level = lvl if lvl > 0 and lvl < maxlvl else maxlvl
-		self.base_atk = lvl1atk + (maxatk-lvl1atk) * (level-1) / (maxlvl-1)
-		self.pot = pot if pot in range(1,7) else 1
-		
-		self.skill = skill if skill in [2] else 2 ###### check implemented skills
-		self.mastery = mastery if mastery in [0,1,2,3] else 3
-		if level != maxlvl: self.name = f"Vulcan Lv{level} P{self.pot} S{self.skill}" #####set op name
-		else: self.name = f"Vulcan P{self.pot} S{self.skill}"
-		if self.mastery == 0: self.name += "L7"
-		elif self.mastery < 3: self.name += f"M{self.mastery}"
-		self.targets = max(1,targets)
-		self.trait = TrTaTaSkMo[0]
-		self.talent1 = TrTaTaSkMo[1]
-		self.talent2 = TrTaTaSkMo[2]
-		self.skilldmg = TrTaTaSkMo[3]
-		self.moduledmg = TrTaTaSkMo[4]
-		
-		self.module = module if module in [0,1] else 1 ##### check valid modules
-		self.module_lvl = module_lvl if module_lvl in [1,2,3] else 3		
-		if level >= maxlvl-30:
-			if self.module == 1: self.name += f" ModX{self.module_lvl}"
-			else: self.name += " no Mod"
-		else: self.module = 0
-
-		if self.targets > 1: self.name += f" {self.targets}targets" ######when op has aoe
-		
-		self.buffs = buffs
+	def __init__(self, pp, *args, **kwargs):
+		super().__init__("Vulcan",pp,[1,2],[1],2,1,1)
+		if self.targets > 1 and self.skill == 2: self.name += f" {self.targets}targets"
 	
 	def skill_dps(self, defense, res):
-		dps = 0
-		atkbuff = self.buffs[0]
-		aspd = self.buffs[2]
-		
-		####the actual skills
-		if self.skill == 2:
-			self.atk_interval = 2 
-			atkbuff += 1.05 + 0.15 * self.mastery
-			final_atk = self.base_atk * (1+atkbuff) + self.buffs[1]
-			
-			hitdmg = np.fmax(final_atk - defense, final_atk * 0.05)
-			dps = hitdmg/(self.atk_interval/(1+aspd/100)) * min(self.targets,2)
+		atkbuff = self.skill_params[0] if self.skill == 2 else 0
+		targets = 2 if self.skill == 2 else 1
+		if self.skill == 2: self.atk_interval = 2
+		final_atk = self.atk * (1 + atkbuff + self.buff_atk) + self.buff_atk_flat
+		hitdmg = np.fmax(final_atk - defense, final_atk * 0.05)
+		dps = hitdmg/self.atk_interval * self.attack_speed/100 * min(self.targets,targets)
 		return dps
 
 class W(Operator):
