ab837454f5fcec3a4923321d50c6599e28adfba0
WhoAteMyCQQkie <128926564+WhoAteMyCQQkie@users.noreply.github.com>
2024-09-11 13:01:51 +0200

    Updated Eunectes

--- damagecalc/damage_formulas.py
+++ damagecalc/damage_formulas.py
@@ -3148,86 +3148,18 @@ class Estelle(Operator):
 		return dps
 
 class Eunectes(Operator):
-	def __init__(self, pp, lvl = 0, pot=-1, skill=-1, mastery = 3, module=-1, module_lvl = 3, targets=1, TrTaTaSkMo=[True,True,True,True,True], buffs=[0,0,0],**kwargs):
-		maxlvl=90
-		lvl1atk = 905  #######including trust
-		maxatk = 1077
-		self.atk_interval = 1.6   #### in seconds
-		level = lvl if lvl > 0 and lvl < maxlvl else maxlvl
-		self.base_atk = lvl1atk + (maxatk-lvl1atk) * (level-1) / (maxlvl-1)
-		self.pot = pot if pot in range(1,7) else 1
-		
-		self.skill = skill if skill in [1,2,3] else 3 ###### check implemented skills
-		self.mastery = mastery if mastery in [0,1,2,3] else 3
-		if level != maxlvl: self.name = f"Eunectes Lv{level} P{self.pot} S{self.skill}" #####set op name
-		else: self.name = f"Eunectes P{self.pot} S{self.skill}"
-		if self.mastery == 0: self.name += "L7"
-		elif self.mastery < 3: self.name += f"M{self.mastery}"
-		self.trait = TrTaTaSkMo[0]
-		self.talent1 = TrTaTaSkMo[1]
-		self.moduledmg = TrTaTaSkMo[4]
-		
-		self.module = module if module in [0,1,2] else 1 ##### check valid modules
-		self.module_lvl = module_lvl if module_lvl in [1,2,3] else 3		
-		if level >= maxlvl-30:
-			if self.module == 1:
-				if self.module_lvl == 3: self.base_atk += 105
-				elif self.module_lvl == 2: self.base_atk += 95
-				else: self.base_atk += 80
-				self.name += f" ModX{self.module_lvl}"
-			elif self.module == 2:
-				if self.module_lvl == 3: self.base_atk += 85
-				elif self.module_lvl == 2: self.base_atk += 80
-				else: self.base_atk += 70
-				self.name += f" ModY{self.module_lvl}"
-			else: self.name += " no Mod"
-		else: self.module = 0
+	def __init__(self, pp, *args, **kwargs):
+		super().__init__("Eunectes",pp,[1,2,3],[1,2],3,1,1)
+		if not self.talent_dmg and self.elite > 0: self.name += " <50%hp"
+		if self.module_dmg and self.module == 2: self.name += " WhileBlocking"
 
-		if not self.talent1: self.name += " <50%hp"
-		if self.moduledmg and self.module == 2: self.name += " WhileBlocking"
-
-		self.buffs = buffs
-			
-	
 	def skill_dps(self, defense, res):
-		dps = 0
-		atkbuff = self.buffs[0]
-		aspd = self.buffs[2]
-		atk_scale = 1
-		
-		#talent/module buffs
-		if self.talent1:
-			atk_scale = 1.15
-			if self.pot > 4 : atk_scale += 0.02
-			if self.module == 2:
-				if self.module_lvl == 2: atk_scale += 0.05
-				if self.module_lvl == 3: atk_scale += 0.08
-		
-		if self.moduledmg and self.module == 2:
-			atkbuff += 0.15
-			
-		####the actual skills
-		if self.skill == 1:
-			atkbuff += 0.25 if self.mastery == 3 else 0.18 + 0.02 * self.mastery
-
-			final_atk = self.base_atk * (1+atkbuff) + self.buffs[1]
-			hitdmg = np.fmax(final_atk * atk_scale - defense, final_atk * atk_scale * 0.05)
-			dps = hitdmg/(self.atk_interval/(1+aspd/100))
-			
-		if self.skill == 2:
-			self.atk_interval = 2
-			atkbuff += 1.8 if self.mastery == 3 else 1.3 + 0.15 * self.mastery
-
-			final_atk = self.base_atk * (1+atkbuff) + self.buffs[1]
-			hitdmg = np.fmax(final_atk * atk_scale - defense, final_atk * atk_scale * 0.05)
-			dps = hitdmg/(self.atk_interval/(1+aspd/100))
-		
-		if self.skill == 3:
-			atkbuff += 1.7 + 0.2 * self.mastery
-
-			final_atk = self.base_atk * (1+atkbuff) + self.buffs[1]
-			hitdmg = np.fmax(final_atk * atk_scale - defense, final_atk * atk_scale * 0.05)
-			dps = hitdmg/(self.atk_interval/(1+aspd/100))
+		atk_scale = self.talent1_params[2] if self.talent_dmg and self.elite > 0 else 1
+		atkbuff = 0.15 if self.module_dmg and self.module == 2 else 0
+		final_atk = self.atk *(1+ self.buff_atk + atkbuff + self.skill_params[0]) + self.buff_atk_flat
+		if self.skill == 2: self.atk_interval = 2
+		hitdmg = np.fmax(final_atk * atk_scale - defense, final_atk * atk_scale * 0.05)
+		dps = hitdmg/self.atk_interval * self.attack_speed/100
 		return dps
 
 class ExecutorAlter(Operator):
