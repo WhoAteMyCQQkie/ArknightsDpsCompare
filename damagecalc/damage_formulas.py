8581096495b05ce3846c2c702e24bc59148d6322
WhoAteMyCQQkie <128926564+WhoAteMyCQQkie@users.noreply.github.com>
2024-09-11 13:14:14 +0200

    Updated Doc

--- damagecalc/damage_formulas.py
+++ damagecalc/damage_formulas.py
@@ -2661,61 +2661,17 @@ class Dobermann(Operator):
 		return dps
 
 class Doc(Operator):
-	def __init__(self, pp, lvl = 0, pot=-1, skill=-1, mastery = 3, module=-1, module_lvl = 3, targets=1, TrTaTaSkMo=[True,True,True,True,True], buffs=[0,0,0],**kwargs):
-		maxlvl=80
-		lvl1atk = 551  ##including trust
-		maxatk = 657 
-		self.atk_interval = 1.05   #### in seconds
-		level = lvl if lvl > 0 and lvl < maxlvl else maxlvl
-		self.base_atk = lvl1atk + (maxatk-lvl1atk) * (level-1) / (maxlvl-1)
-		self.pot = pot if pot in range(1,7) else 1
-		
-		self.skill = skill if skill in [1] else 1 ###### check implemented skills
-		self.mastery = mastery if mastery in [0,1,2,3] else 3
-		if level != maxlvl: self.name = f"Doc Lv{level} P{self.pot} S{self.skill}" #####set op name
-		else: self.name = f"Doc P{self.pot} S{self.skill}"
-		if self.mastery == 0: self.name += "L7"
-		elif self.mastery < 3: self.name += f"M{self.mastery}"
-		self.targets = max(1,targets)
-		self.trait = TrTaTaSkMo[0]
-		
-		self.module = module if module in [0,2] else 2 ##### check valid modules
-		self.module_lvl = module_lvl if module_lvl in [1,2,3] else 3		
-		if level >= maxlvl-30:
-			if self.module == 2:
-				if self.module_lvl == 3: self.base_atk += 35
-				elif self.module_lvl == 2: self.base_atk += 30
-				else: self.base_atk += 25
-				self.name += f" ModY{self.module_lvl}"
-			else: self.name += " no Mod"
-		else: self.module = 0
-		
-		if not self.trait: self.name += " blocking"   ##### keep the ones that apply
-		
-		self.buffs = buffs
-			
-	
+	def __init__(self, pp, *args, **kwargs):
+		super().__init__("Doc",pp,[1],[2],1,1,2)
+		if not self.trait_dmg: self.name += " blocking"
+
 	def skill_dps(self, defense, res):
-		dps = 0
-		atkbuff = self.buffs[0]
-		aspd = self.buffs[2]
-		atk_scale = 1
-		
-		#talent/module buffs
-		defignore = 140 if self.pot > 4 else 120
-		if self.module == 2 and self.module_lvl > 1:
-			defignore += 30 * (self.module_lvl -1)
-		newdef = np.fmax(0, defense-defignore)
-		if self.trait: atk_scale = 1.2
-			
-		####the actual skills
-		if self.skill == 1:
-			self.atk_interval = 0.35
-			
-			final_atk = self.base_atk * (1+atkbuff) + self.buffs[1]
-			
-			hitdmg = np.fmax(final_atk * atk_scale - newdef, final_atk * atk_scale * 0.05)
-			dps = hitdmg/(self.atk_interval/(1+aspd/100))
+		atk_scale = 1.2 if self.trait_dmg else 1
+		newdef = np.fmax(0, defense-self.talent1_params[1])
+		atk_interval = self.atk_interval + self.skill_params[3]
+		final_atk = self.atk * (1 + self.buff_atk) + self.buff_atk_flat
+		hitdmg = np.fmax(final_atk * atk_scale - newdef, final_atk * atk_scale * 0.05)
+		dps = hitdmg/atk_interval * self.attack_speed/100
 		return dps
 
 class Dorothy(Operator):
