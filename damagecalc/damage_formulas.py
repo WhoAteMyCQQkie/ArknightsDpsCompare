4c3d6794bd057626467ff791a64f8a1b5f950dde
WhoAteMyCQQkie <128926564+WhoAteMyCQQkie@users.noreply.github.com>
2024-09-18 16:32:41 +0200

    Updated Lappland

--- damagecalc/damage_formulas.py
+++ damagecalc/damage_formulas.py
@@ -5272,77 +5272,27 @@ class LaPluma(Operator):
 		return dps
 	
 class Lappland(Operator):
-	def __init__(self, pp, lvl = 0, pot=-1, skill=-1, mastery = 3, module=-1, module_lvl = 3, targets=1, TrTaTaSkMo=[True,True,True,True,True], buffs=[0,0,0],**kwargs):
-		maxlvl=80
-		lvl1atk = 629  #######including trust
-		maxatk = 760
-		self.atk_interval = 1.3   #### in seconds
-		level = lvl if lvl > 0 and lvl < maxlvl else maxlvl
-		self.base_atk = lvl1atk + (maxatk-lvl1atk) * (level-1) / (maxlvl-1)
-		self.pot = pot if pot in range(1,7) else 1
-		if self.pot > 3: self.base_atk += 25
-		
-		self.skill = skill if skill in [1,2] else 2 ###### check implemented skills
-		self.mastery = mastery if mastery in [0,1,2,3] else 3
-		if level != maxlvl: self.name = f"Lappland Lv{level} P{self.pot} S{self.skill}" #####set op name
-		else: self.name = f"Lappland P{self.pot} S{self.skill}"
-		if self.mastery == 0: self.name += "L7"
-		elif self.mastery < 3: self.name += f"M{self.mastery}"
-		self.targets = max(1,targets)
-		self.trait = TrTaTaSkMo[0]
-		
-		self.moduledmg = TrTaTaSkMo[4]
-		
-		self.module = module if module in [0,1] else 1 ##### check valid modules
-		self.module_lvl = module_lvl if module_lvl in [1,2,3] else 3		
-		if level >= maxlvl-30:
-			if self.module == 1:
-				if self.module_lvl == 3: self.base_atk += 40
-				elif self.module_lvl == 2: self.base_atk += 32
-				else: self.base_atk += 22
-				self.name += f" ModX{self.module_lvl}"
-			else: self.name += " no Mod"
-		else: self.module = 0
-		
-		if not self.trait and self.skill == 1: self.name += " rangedAtk"   ##### keep the ones that apply
-		
+	def __init__(self, pp, *args, **kwargs):
+		super().__init__("Lappland",pp,[1,2],[1],2,1,1)
+		if not self.trait_dmg and self.skill == 1: self.name += " rangedAtk"   ##### keep the ones that apply
 		if self.targets > 1 and self.skill == 2: self.name += f" {self.targets}targets" ######when op has aoe
-		
-		self.buffs = buffs
-			
 	
 	def skill_dps(self, defense, res):
-		dps = 0
-		atkbuff = self.buffs[0]
-		aspd = self.buffs[2]
-		atk_scale = 1
-		
-		if not self.trait and self.skill == 1:
-			atk_scale = 0.8
+		atk_scale = 0.8 if not self.trait_dmg and self.skill == 1 else 1
 		bonus = 0.1 if self.module == 1 else 0
-		if self.module == 1:
-			aspd += 3 + self.module_lvl
-		
-		fragile = 0
-		if self.module == 1:
-			fragile = 0.04 * (self.module_lvl -1)
-		fragile = max(fragile, self.buffs[3])
+		fragile = 0.04 * (self.module_lvl-1) if self.module == 1 and self.module_lvl > 1 else 0
+		fragile = max(fragile, self.buff_fragile)
+		final_atk = self.atk * (1 + self.buff_atk + self.skill_params[0]) + self.buff_atk_flat
 		####the actual skills
 		if self.skill == 1:
-			atkbuff += 0.55 + 0.05* self.mastery
-
-			final_atk = self.base_atk * (1+atkbuff) + self.buffs[1]
-			hitdmg = np.fmax(final_atk *atk_scale - defense, final_atk* atk_scale * 0.05)
+			hitdmg = np.fmax(final_atk * atk_scale - defense, final_atk * atk_scale * 0.05)
 			bonusdmg = np.fmax(final_atk * bonus *(1-res/100), final_atk * bonus * 0.05)
-			
-			dps = hitdmg/(self.atk_interval/(1+aspd/100)) + bonusdmg/(self.atk_interval/(1+aspd/100))
+			dps = (hitdmg + bonusdmg) / self.atk_interval * self.attack_speed/100
 		if self.skill == 2:
-			atkbuff += 0.9 + 0.1 * self.mastery
-			final_atk = self.base_atk * (1+atkbuff) + self.buffs[1]
-			hitdmgarts = np.fmax(final_atk * (1-res/100), final_atk * 0.05)
+			hitdmg = np.fmax(final_atk * (1-res/100), final_atk * 0.05)
 			bonusdmg = np.fmax(final_atk * bonus *(1-res/100), final_atk * bonus * 0.05)
-			dps = hitdmgarts/(self.atk_interval/(1+aspd/100)) * min(2,self.targets) + bonusdmg/(self.atk_interval/(1+aspd/100)) * min(2,self.targets)
-		return dps*(1+fragile)/(1+self.buffs[3])
+			dps = (hitdmg + bonusdmg) / self.atk_interval * self.attack_speed/100 * min(2,self.targets)
+		return dps*(1+fragile)/(1+self.buff_fragile)
 	
 class Lavaalt(Operator):
 	def __init__(self, pp, lvl = 0, pot=-1, skill=-1, mastery = 3, module=-1, module_lvl = 3, targets=1, TrTaTaSkMo=[True,True,True,True,True], buffs=[0,0,0],**kwargs):
