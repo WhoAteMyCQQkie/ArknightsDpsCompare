d3d2fecab818e80882c49e0c0ab95d68ab9cde14
WhoAteMyCQQkie <128926564+WhoAteMyCQQkie@users.noreply.github.com>
2024-09-19 14:12:00 +0200

    bugfix: op_params parser with plot_params input

--- damagecalc/utils.py
+++ damagecalc/utils.py
@@ -124,9 +124,9 @@ def parse_plot_parameters(pps: PlotParametersSet, args: list[str]):
 			pps.pots = {-1}
 		elif arg in ["modlvl1","modlvl2","modlvl3","modlv1","modlv2","modlv3"]:
 			pps.module_lvls = {-1}
-		elif arg in ["1","2","3"] and previous_arg not in ["skilllvl","skilllevel","skilllv","skillvl","skillevel","skillv","slv","slevel","slvl","lvl","level","lv","t","target","targets","trust","hit","hits","r","res","resis","resistance","d","def","defense"]:
+		elif arg in ["1","2","3"] and previous_arg not in ["skilllvl","skilllevel","skilllv","skillvl","skillevel","skillv","slv","slevel","slvl","lvl","level","lv","t","target","targets","trust","hit","hits","r","res","resis","resistance","d","def","defense","fixres","fixdef","fix"]:
 			pps.module_lvls = {-1}
-		elif arg in ["0"] and previous_arg not in ["skilllvl","skilllevel","skilllv","skillvl","skillevel","skillv","slv","slevel","slvl","lvl","level","lv","t","target","targets","trust","hit","hits","r","res","resis","resistance","d","def","defense"]:
+		elif arg in ["0"] and previous_arg not in ["skilllvl","skilllevel","skilllv","skillvl","skillevel","skillv","slv","slevel","slvl","lvl","level","lv","t","target","targets","trust","hit","hits","r","res","resis","resistance","d","def","defense","fixres","fixdef","fix"]:
 			pps.modules = {-1}
 		elif arg in ["mod0","modx","x","mody","y","modd","d","no","mod","nomod"]:
 			pps.modules = {-1}
@@ -144,7 +144,9 @@ def parse_plot_parameters(pps: PlotParametersSet, args: list[str]):
 	entries = len(args)
 	unused_inputs = set()
 	while i < entries:
-		if args[i] in ["s1","s2","s3"]: 
+		if args[i] in ["skilllvl","skilllevel","skilllv","skillvl","skillevel","skillv","slv","slevel","slvl","lvl","level","lv","t","target","targets","trust","hit","hits","r","res","resis","resistance","d","def","defense","fixres","fixdef","fix"]:
+			i += 1
+		elif args[i] in ["s1","s2","s3"]: 
 			pps.skills.add(int(args[i][1]))
 			pps.skills.discard(-1)
 		elif args[i] in ["e0","e1","e2"]: 
@@ -228,10 +230,10 @@ def parse_plot_parameters(pps: PlotParametersSet, args: list[str]):
 				else:
 					try:
 						val = float(args[i])
-						if val > 0 and val < 1:
+						if val > 0 and val < 2:
 							pps.buffs[0] = val
-						if val > 2:
-							pps.buffs[1] = val
+						if val >= 2:
+							pps.buffs[1] = int(val)
 					except ValueError:
 						break
 				i+=1
