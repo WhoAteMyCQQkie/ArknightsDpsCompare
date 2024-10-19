ac6ce1a2410d567dca85e5caf9782314f45b386e
WhoAteMyCQQkie <128926564+WhoAteMyCQQkie@users.noreply.github.com>
2024-10-19 22:22:23 +0200

    Updated Andreana

--- damagecalc/damage_formulas.py
+++ damagecalc/damage_formulas.py
@@ -566,74 +566,15 @@ class AmiyaGuard(Operator):
 		return dps
 
 class Andreana(Operator):
-	def __init__(self, pp, lvl = 0, pot=-1, skill=-1, mastery = 3, module=-1, module_lvl = 3, targets=1, TrTaTaSkMo=[True,True,True,True,True], buffs=[0,0,0],**kwargs):
-		maxlvl=80
-		lvl1atk = 917  #######including trust
-		maxatk = 1110
-		self.atk_interval = 2.7   #### in seconds
-		level = lvl if lvl > 0 and lvl < maxlvl else maxlvl
-		self.base_atk = lvl1atk + (maxatk-lvl1atk) * (level-1) / (maxlvl-1)
-		self.pot = pot if pot in range(1,7) else 1
-		if self.pot > 3: self.base_atk += 35
-		
-		self.skill = skill if skill in [1,2] else 2 ###### check implemented skills
-		self.mastery = mastery if mastery in [0,1,2,3] else 3
-		if level != maxlvl: self.name = f"Andreana Lv{level} P{self.pot} S{self.skill}" #####set op name
-		else: self.name = f"Andreana P{self.pot} S{self.skill}"
-		if self.mastery == 0: self.name += "L7"
-		elif self.mastery < 3: self.name += f"M{self.mastery}"
-		self.skilldmg = TrTaTaSkMo[3]
-		self.moduledmg = TrTaTaSkMo[4]
-		
-		self.module = module if module in [0,1] else 1 ##### check valid modules
-		self.module_lvl = module_lvl if module_lvl in [1,2,3] else 3		
-		if level >= maxlvl-30:
-			if self.module == 1:
-				if self.module_lvl == 3: self.base_atk += 91
-				elif self.module_lvl == 2: self.base_atk += 81
-				else: self.base_atk += 65
-				self.name += f" ModX{self.module_lvl}"
-			else: self.name += " no Mod"
-		else: self.module = 0
-		
-		if self.moduledmg and self.module == 1: self.name += " atMaxRange"
-
-		
-		self.buffs = buffs
+	def __init__(self, pp, *args, **kwargs):
+		super().__init__("Andreana",pp,[1,2],[1],2,1,1)
+		if self.module_dmg and self.module == 1: self.name += " atMaxRange"
 			
-	
 	def skill_dps(self, defense, res):
-		dps = 0
-		atkbuff = self.buffs[0]
-		aspd = self.buffs[2]
-		atk_scale = 1
-		
-		#talent/module buffs
-		aspd += 12
-		if self.pot > 4: aspd += 2
-
-		if self.module == 1:
-			if self.module_lvl == 3 : aspd += 8
-			elif self.module_lvl == 2: aspd += 5
-			if self.moduledmg: atk_scale = 1.15
-			
-		####the actual skills
-		if self.skill == 1:
-			atkbuff += 0.6+ 0.15 * self.mastery
-			if self.mastery == 3: atkbuff -= 0.05
-			final_atk = self.base_atk * (1+atkbuff) + self.buffs[1]
-			hitdmg = np.fmax(final_atk *atk_scale - defense, final_atk* atk_scale * 0.05)
-			
-			dps = hitdmg/(self.atk_interval/(1+aspd/100))
-		
-		if self.skill == 2:
-			atkbuff += 1.9+ 0.15 * self.mastery
-			if self.mastery == 3: atkbuff += 0.05
-			final_atk = self.base_atk * (1+atkbuff) + self.buffs[1]
-			hitdmg = np.fmax(final_atk *atk_scale - defense, final_atk* atk_scale * 0.05)
-			
-			dps = hitdmg/(self.atk_interval/(1+aspd/100))	
-		
+		atk_scale = 1.15 if self.module == 1 and self.module_dmg else 1
+		final_atk = self.atk * (1 + self.buff_atk + self.skill_params[0]) + self.buff_atk_flat
+		hitdmg = np.fmax(final_atk *atk_scale - defense, final_atk* atk_scale * 0.05)
+		dps = hitdmg/self.atk_interval * (self.attack_speed + self.talent1_params[0])/100
 		return dps
 
 class Angelina(Operator):
