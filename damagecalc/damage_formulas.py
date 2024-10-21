dcef0e07ef7b676f13ee5303a7856c5042c8247e
WhoAteMyCQQkie <128926564+WhoAteMyCQQkie@users.noreply.github.com>
2024-10-21 16:32:43 +0200

    Updated Aak

--- damagecalc/damage_formulas.py
+++ damagecalc/damage_formulas.py
@@ -347,65 +347,27 @@ class twelveF(Operator):
 		return dps
 
 class Aak(Operator):
-	def __init__(self, pp, lvl = 0, pot=-1, skill=-1, mastery = 3, module=-1, module_lvl = 3, targets=1, TrTaTaSkMo=[True,True,True,True,True], buffs=[0,0,0,0,0],**kwargs):
-		maxlvl=90
-		lvl1atk = 633  #######including trust
-		maxatk = 753
-		self.atk_interval = 1.6   #### in seconds
-		level = lvl if lvl > 0 and lvl < maxlvl else maxlvl
-		self.base_atk = lvl1atk + (maxatk-lvl1atk) * (level-1) / (maxlvl-1)
-		self.pot = pot if pot in range(1,7) else 1
-		
-		self.skill = skill if skill in [1,3] else 1 ###### check implemented skills
-		self.mastery = mastery if mastery in [0,1,2,3] else 3
-		if level != maxlvl: self.name = f"Aak Lv{level} P{self.pot} S{self.skill}" #####set op name
-		else: self.name = f"Aak P{self.pot} S{self.skill}"
-		if self.mastery == 0: self.name += "L7"
-		elif self.mastery < 3: self.name += f"M{self.mastery}"
-
-		
-		self.module = module if module in [0,1,2] else 1 ##### check valid modules
-		self.module_lvl = module_lvl if module_lvl in [1,2,3] else 3		
-		if level >= maxlvl-30:
-			if self.module == 1:
-				if self.module_lvl == 3: self.base_atk += 57
-				elif self.module_lvl == 2: self.base_atk += 47
-				else: self.base_atk += 37
-				self.name += f" ModX{self.module_lvl}"
-			else: self.name += " no Mod"
-		else: self.module = 0
-
-		self.buffs = buffs
-			
+	def __init__(self, pp, *args, **kwargs):
+		super().__init__("Aak",pp,[1,3],[1],1,1,1)
 	
 	def skill_dps(self, defense, res):
-		dps = 0
-		atkbuff = self.buffs[0]
-		aspd = self.buffs[2]
-
-		#talent/module buffs
-		cdmg = 1.5
+		cdmg = max(self.talent1_params)
+		crate = 0.25
 		if self.module == 1 and self.module_lvl > 1:
-			cdmg += 0.05 * self.module_lvl
-		
-		####the actual skills
+			crate = 0.25 + 0.75 * 0.2 if self.module_lvl == 2 else 0.25 + 0.75 * 0.3
+
 		if self.skill == 1:
-			aspd += 70 + 10 * self.mastery
-			
-			final_atk = self.base_atk * (1+atkbuff) + self.buffs[1]
-			
-			hitdmg = np.fmax(final_atk - defense, final_atk * 0.05)
-			critdmg = np.fmax(final_atk * cdmg - defense, final_atk * cdmg * 0.05)
-			dps = (0.75*hitdmg+0.25*critdmg)/(self.atk_interval/(1+aspd/100))
+			aspd = self.skill_params[0]
+			final_atk = self.atk * (1 + self.buff_atk) + self.buff_atk_flat
 		if self.skill == 3:
-			aspd += 35 + 5 * self.mastery
-			atkbuff += 0.35 + 0.05 * self.mastery
-			
-			final_atk = self.base_atk * (1+atkbuff) + self.buffs[1]
-			
-			hitdmg = np.fmax(final_atk - defense, final_atk * 0.05)
-			critdmg = np.fmax(final_atk * cdmg - defense, final_atk * cdmg * 0.05)
-			dps =  (0.75*hitdmg+0.25*critdmg)/(self.atk_interval/(1+aspd/100))
+			aspd = self.skill_params[1]
+			atkbuff = self.skill_params[0]
+			final_atk = self.atk * (1 + self.buff_atk + atkbuff) + self.buff_atk_flat
+		hitdmg = np.fmax(final_atk - defense, final_atk * 0.05)
+		critdmg = np.fmax(final_atk * cdmg - defense, final_atk * cdmg * 0.05)
+		print(aspd, final_atk)
+		avghit = (1-crate)*hitdmg + crate * critdmg
+		dps = avghit/self.atk_interval * (self.attack_speed + aspd)/100
 		return dps
 
 class Absinthe(Operator):
