ca2183d70cfd318e312820704658840365fde02d
WhoAteMyCQQkie <128926564+WhoAteMyCQQkie@users.noreply.github.com>
2024-10-22 22:05:29 +0200

    Updated Iana

--- damagecalc/damage_formulas.py
+++ damagecalc/damage_formulas.py
@@ -4274,62 +4274,23 @@ class Humus(Operator):
 		return dps
 
 class Iana(Operator):
-	def __init__(self, pp, lvl = 0, pot=-1, skill=-1, mastery = 3, module=-1, module_lvl = 3, targets=1, TrTaTaSkMo=[True,True,True,True,True], buffs=[0,0,0],**kwargs):
-		maxlvl=90
-		lvl1atk = 630  #######including trust
-		maxatk = 758
-		self.atk_interval = 1.2   #### in seconds
-		level = lvl if lvl > 0 and lvl < maxlvl else maxlvl
-		self.base_atk = lvl1atk + (maxatk-lvl1atk) * (level-1) / (maxlvl-1)
-		self.pot = pot if pot in range(1,7) else 1
-		if self.pot > 3: self.base_atk += 27
-		
-		self.skill = skill if skill in [2] else 2 ###### check implemented skills
-		self.mastery = mastery if mastery in [0,1,2,3] else 3
-		if level != maxlvl: self.name = f"Iana Lv{level} P{self.pot} S{self.skill}" #####set op name
-		else: self.name = f"Iana P{self.pot} S{self.skill}"
-		if self.mastery == 0: self.name += "L7"
-		elif self.mastery < 3: self.name += f"M{self.mastery}"
-		
-		self.module = module if module in [0,1] else 1 ##### check valid modules
-		self.module_lvl = module_lvl if module_lvl in [1,2,3] else 3		
-		if level >= maxlvl-30:
-			if self.module == 1:
-				if self.module_lvl == 3: self.base_atk += 65
-				elif self.module_lvl == 2: self.base_atk += 55
-				else: self.base_atk += 45
-				self.name += f" ModX{self.module_lvl}"
-			else: self.name += " no Mod"
-		else: self.module = 0
-	
-		self.buffs = buffs
+	def __init__(self, pp, *args, **kwargs):
+		super().__init__("Iana",pp,[1,2],[1],2,1,1)
+		if self.skill == 1:
+			atkbuff = 0.15 if self.module == 1 else 0
+			final_atk = self.atk * (1 + atkbuff + self.buff_atk) + self.buff_atk_flat
+			fragile = max(self.talent1_params[2] - 1, self.buff_fragile)
+			nukedmg = final_atk * self.skill_params[0] * (1+fragile)
+			self.name += f" InitialDmg:{int(nukedmg)}"
 			
-	
 	def skill_dps(self, defense, res):
-		dps = 0
-		atkbuff = self.buffs[0]
-		aspd = self.buffs[2]
-		atk_scale = 1
-		
-		#talent/module buffs
-		fragile = 0.22 if self.pot > 4 else 0.2
-		if self.module == 1:
-			atkbuff += 0.15  #from module change, since she only deals damage as doll
-			if self.module_lvl == 2: fragile += 0.05
-			if self.module_lvl == 3: fragile += 0.08
-
-			
-		####the actual skills
-		if self.skill == 2:
-			aspd += 300 if self.mastery == 3 else 260 + 10 * self.mastery
-			
-			fragile = max(fragile, self.buffs[3])
-
-			final_atk = self.base_atk * (1+atkbuff) + self.buffs[1]
-			
-			hitdmg = np.fmax(final_atk - defense, final_atk * 0.05) * (1+fragile)
-
-			dps = hitdmg/(self.atk_interval/(1+aspd/100)) /(1+self.buffs[3])
+		atkbuff = 0.15 if self.module == 1 else 0
+		fragile = self.talent1_params[2] - 1
+		fragile = max(fragile, self.buff_fragile)
+		aspd = self.skill_params[0] if self.skill == 2 else 0
+		final_atk = self.atk * (1 + atkbuff + self.buff_atk) + self.buff_atk_flat
+		hitdmg = np.fmax(final_atk - defense, final_atk * 0.05) * (1+fragile)
+		dps = hitdmg/self.atk_interval * (self.attack_speed+aspd)/100 /(1+self.buff_fragile)
 		return dps
 
 class Ifrit(Operator):
