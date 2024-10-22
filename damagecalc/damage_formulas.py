b6d565b769dfb29890d33d1aa0af2de097e71716
WhoAteMyCQQkie <128926564+WhoAteMyCQQkie@users.noreply.github.com>
2024-10-22 18:13:16 +0200

    Updated Bagpipe

--- damagecalc/damage_formulas.py
+++ damagecalc/damage_formulas.py
@@ -964,113 +964,57 @@ class Aurora(Operator):
 		return dps
 		
 class Bagpipe(Operator):
-	def __init__(self, pp, lvl = 0, pot=-1, skill=-1, mastery = 3, module=-1, module_lvl = 3, targets=1, TrTaTaSkMo=[True,True,True,True,True], buffs=[0,0,0],**kwargs):
-		maxlvl=90
-		lvl1atk = 578  #######including trust
-		maxatk = 671
-		self.atk_interval = 1  #### in seconds
-		level = lvl if lvl > 0 and lvl < maxlvl else maxlvl
-		self.base_atk = lvl1atk + (maxatk-lvl1atk) * (level-1) / (maxlvl-1)
-		self.pot = pot if pot in range(1,7) else 1
-		if self.pot > 3: self.base_atk += 25
-		
-		self.skill = skill if skill in [1,2,3] else 3 ###### check implemented skills
-		self.mastery = mastery if mastery in [0,1,2,3] else 3
-		if level != maxlvl: self.name = f"Bagpipe Lv{level} P{self.pot} S{self.skill}" #####set op name
-		else: self.name = f"Bagpipe P{self.pot} S{self.skill}"
-		if self.mastery == 0: self.name += "L7"
-		elif self.mastery < 3: self.name += f"M{self.mastery}"
-		self.targets = max(1,targets)
-		self.moduledmg = TrTaTaSkMo[4]
-		
-		self.module = module if module in [0,1,2] else 1 ##### check valid modules
-		self.module_lvl = module_lvl if module_lvl in [1,2,3] else 3		
-		if level >= maxlvl-30:
-			if self.module == 1:
-				if self.module_lvl == 3: self.base_atk += 73
-				elif self.module_lvl == 2: self.base_atk += 64
-				else: self.base_atk += 50
-				self.name += f" ModX{self.module_lvl}"
-			elif self.module == 2:
-				if self.module_lvl == 3: self.base_atk += 85
-				elif self.module_lvl == 2: self.base_atk += 75
-				else: self.base_atk += 65
-				self.name += f" ModY{self.module_lvl}"
-			else: self.name += " no Mod"
-		else: self.module = 0
-		
-		if self.moduledmg and self.module == 2: self.name += " lowHpTarget"
-		
-		if self.targets > 1: self.name += f" {self.targets}targets" ######when op has aoe
-		
-		self.buffs = buffs
-			
+	def __init__(self, pp, *args, **kwargs):
+		super().__init__("Bagpipe",pp,[1,2,3],[1,2],3,1,1)
+		if self.module_dmg and self.module == 2: self.name += " lowHpTarget"
+		if self.targets > 1: self.name += f" {self.targets}targets"	
 	
 	def skill_dps(self, defense, res):
-		dps = 0
-		atkbuff = self.buffs[0]
-		aspd = self.buffs[2]
-		atk_scale = 1
-		
-		crate = 0.25
-		if self.pot > 2: crate += 0.03
-		cdmg = 1.3
-		if self.module == 2:
-			cdmg += 0.05 * (self.module_lvl -1)
-			if self.module_lvl == 2: crate += 0.05
-			if self.module_lvl == 3: crate += 0.08
-			if self.moduledmg: atk_scale = 1.15
+		atk_scale = 1.15 if self.module == 2 and self.module_dmg else 1
+		crate = self.talent1_params[1] if self.elite > 0 else 0
+		cdmg = self.talent1_params[0] if self.elite > 0 else 1
 
-			
-		####the actual skills
 		if self.skill == 1:
-			atkbuff += 0.34 + 0.03 * self.mastery
-			if self.mastery == 3: atkbuff+= 0.02
-			aspd += 45 if self.mastery == 3 else 35
-
-			final_atk = self.base_atk * (1+atkbuff) + self.buffs[1]
-			
-			hitdmg = np.fmax(final_atk *atk_scale - defense, final_atk* atk_scale * 0.05)
-			critdmg = np.fmax(final_atk *atk_scale *cdmg - defense, final_atk* atk_scale * cdmg * 0.05)
+			atkbuff = self.skill_params[0]
+			aspd = self.skill_params[1]
+			final_atk = self.atk * (1 + atkbuff + self.buff_atk) + self.buff_atk_flat
+			hitdmg = np.fmax(final_atk * atk_scale - defense, final_atk * atk_scale * 0.05)
+			critdmg = np.fmax(final_atk * atk_scale *cdmg - defense, final_atk * atk_scale * cdmg * 0.05)
 			avgdmg = crate * critdmg * min(2, self.targets) + (1-crate) * hitdmg
-			dps = avgdmg/(self.atk_interval/(1+aspd/100))
+			dps = avgdmg/self.atk_interval * (self.attack_speed + aspd)/100
 		
 		if self.skill == 2:
-			sp_cost = 4 if self.mastery == 3 else 5
-			if self.mastery == 3: skill_scale = 2
-			else: skill_scale = 1.6 + 0.1 * self.mastery
-			
-			final_atk = self.base_atk * (1+atkbuff) + self.buffs[1]
-			
-			hitdmg = np.fmax(final_atk *atk_scale - defense, final_atk* atk_scale * 0.05)
-			critdmg = np.fmax(final_atk *atk_scale *cdmg - defense, final_atk* atk_scale * cdmg * 0.05)
-			
-			skillhit = np.fmax(final_atk *atk_scale * skill_scale - defense, final_atk* atk_scale * skill_scale * 0.05)
-			skillcrit = np.fmax(final_atk *atk_scale * skill_scale *cdmg - defense, final_atk* atk_scale * skill_scale * cdmg * 0.05)
+			skill_scale = self.skill_params[0]
+			final_atk = self.atk * (1 + self.buff_atk) + self.buff_atk_flat
+			hitdmg = np.fmax(final_atk * atk_scale - defense, final_atk * atk_scale * 0.05)
+			critdmg = np.fmax(final_atk * atk_scale *cdmg - defense, final_atk * atk_scale * cdmg * 0.05)
+			skillhit = np.fmax(final_atk * atk_scale * skill_scale - defense, final_atk * atk_scale * skill_scale * 0.05)
+			skillcrit = np.fmax(final_atk * atk_scale * skill_scale *cdmg - defense, final_atk * atk_scale * skill_scale * cdmg * 0.05)
 			
 			avgdmg = crate * critdmg * min(2, self.targets) + (1-crate) * hitdmg
 			avgskill = crate * skillcrit * min(2, self.targets) + (1-crate) * skillhit
 			avgskill *= 2
-			
-			sp_cost = sp_cost + 1.2 #sp lockout
-			atkcycle = self.atk_interval/(1+aspd/100)
+			sp_cost = self.skill_cost / (1 + self.sp_boost) + 1.2 #sp lockout
+			atkcycle = self.atk_interval/(self.attack_speed/100)
 			atks_per_skillactivation = sp_cost / atkcycle
-			
 			avghit = avgskill
-			if atks_per_skillactivation > 1:
-				avghit = (avgskill + (atks_per_skillactivation - 1) * hitdmg) / atks_per_skillactivation	
 			
-			dps = avghit/(self.atk_interval/(1+aspd/100))
-		
+			if atks_per_skillactivation > 1:
+				if self.skill_params[1] > 1:
+					avghit = (avgskill + (atks_per_skillactivation - 1) * avgdmg) / atks_per_skillactivation
+				else:
+					avghit = (avgskill + int(atks_per_skillactivation) * avgdmg) / (int(atks_per_skillactivation)+1)
+			
+			dps = avghit/self.atk_interval * self.attack_speed/100
 		
 		if self.skill == 3:
-			atkbuff += 0.9 + 0.1 * self.mastery
+			atkbuff = self.skill_params[0]
 			self.atk_interval = 1.7
-			final_atk = self.base_atk * (1+atkbuff) + self.buffs[1]
-			hitdmg = np.fmax(final_atk *atk_scale - defense, final_atk* atk_scale * 0.05)
-			critdmg = np.fmax(final_atk *atk_scale *cdmg - defense, final_atk* atk_scale * cdmg * 0.05)
+			final_atk = self.atk * (1 + atkbuff + self.buff_atk) + self.buff_atk_flat
+			hitdmg = np.fmax(final_atk * atk_scale - defense, final_atk * atk_scale * 0.05)
+			critdmg = np.fmax(final_atk * atk_scale *cdmg - defense, final_atk * atk_scale * cdmg * 0.05)
 			avgdmg = crate * critdmg * min(2, self.targets) + (1-crate) * hitdmg
-			dps = 3*avgdmg/(self.atk_interval/(1+aspd/100)) 
+			dps = 3 * avgdmg/self.atk_interval * self.attack_speed/100
 		return dps
 	
 class Beehunter(Operator):
