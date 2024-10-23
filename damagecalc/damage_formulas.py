f6fc8195b41268dcb209ed046a3cabdd9d17498c
WhoAteMyCQQkie <128926564+WhoAteMyCQQkie@users.noreply.github.com>
2024-10-23 12:31:53 +0200

    Updated Click

--- damagecalc/damage_formulas.py
+++ damagecalc/damage_formulas.py
@@ -1599,71 +1599,18 @@ class CivilightEterna(Operator):
 		return dps
 
 class Click(Operator):
-	def __init__(self, pp, lvl = 0, pot=-1, skill=-1, mastery = 3, module=-1, module_lvl = 3, targets=1, TrTaTaSkMo=[True,True,True,True,True], buffs=[0,0,0],**kwargs):
-		maxlvl=70
-		lvl1atk = 324  #######including trust
-		maxatk = 375
-		self.atk_interval = 1.3   #### in seconds
-		level = lvl if lvl > 0 and lvl < maxlvl else maxlvl
-		self.base_atk = lvl1atk + (maxatk-lvl1atk) * (level-1) / (maxlvl-1)
-		self.pot = pot if pot in range(1,7) else 6
-		
-		self.skill = skill if skill in [1,2] else 2 ###### check implemented skills
-		self.mastery = mastery if mastery in [0,1,2,3] else 3
-		if level != maxlvl: self.name = f"Click Lv{level} P{self.pot} S{self.skill}" #####set op name
-		else: self.name = f"Click P{self.pot} S{self.skill}"
-		if self.mastery == 0: self.name += "L7"
-		elif self.mastery < 3: self.name += f"M{self.mastery}"
-		
-		self.trait = TrTaTaSkMo[0]
-		self.talent = TrTaTaSkMo[1]
-		self.skilldmg = TrTaTaSkMo[3]
-		self.moduledmg = TrTaTaSkMo[4]
-		
-		self.module = module if module in [0,2] else 2 ##### check valid modules
-		self.module_lvl = module_lvl if module_lvl in [1,2,3] else 3		
-		if level >= maxlvl-30:
-			if self.module == 2:
-				if self.module_lvl == 3: self.base_atk += 18
-				elif self.module_lvl == 2: self.base_atk += 15
-				else: self.base_atk += 13
-				self.name += f" ModY{self.module_lvl}"
-			else: self.name += " no Mod"
+	def __init__(self, pp, *args, **kwargs):
+		super().__init__("Click",pp,[1,2],[2],2,6,2)
+		if not self.trait_dmg: self.name += " minDroneDmg"
 
-		else: self.module = 0
-		
-		if not self.trait: self.name += " minDroneDmg"
-		
-		self.buffs = buffs		
-	
 	def skill_dps(self, defense, res):
-		dps = 0
-		atkbuff = self.buffs[0]
-		aspd = self.buffs[2]
-		if self.pot > 3: aspd +=6
-		aspd += 12
-		
+		aspd = self.talent1_params[0]
 		drone_dmg = 1.2 if self.module == 2 else 1.1
-		
-		if self.module == 2: 
-			if self.module_lvl == 2:aspd += 3
-			if self.module_lvl == 3:aspd += 6 
-
-		if not self.trait:
-			drone_dmg = 0.2
-		
-		if self.skill == 1:
-			atkbuff += 0.5 + 0.1 * self.mastery
-		else:
-			atkbuff += 0.58 + 0.04 * self.mastery
-
-		final_atk = self.base_atk * (1+atkbuff) + self.buffs[1]
-		drone_atk = drone_dmg * final_atk
-		
-		dmgperinterval = final_atk + drone_atk
-		
+		if not self.trait_dmg: drone_dmg = 0.2
+		final_atk = self.atk * (1 + self.buff_atk + self.skill_params[0]) + self.buff_atk_flat
+		dmgperinterval = final_atk + drone_dmg * final_atk
 		hitdmgarts = np.fmax(dmgperinterval *(1-res/100), dmgperinterval * 0.05)
-		dps = hitdmgarts/(self.atk_interval/(1+aspd/100))
+		dps = hitdmgarts/self.atk_interval * (self.attack_speed + aspd) / 100
 		return dps
 
 class Coldshot(Operator):
