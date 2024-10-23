22b5270c010b04a4dc0050bdd28ba9e75fa4e3f0
WhoAteMyCQQkie <128926564+WhoAteMyCQQkie@users.noreply.github.com>
2024-10-23 12:21:44 +0200

    Updated Broca, finishing Operators with B

--- damagecalc/damage_formulas.py
+++ damagecalc/damage_formulas.py
@@ -1071,81 +1071,20 @@ class BluePoison(Operator):
 		return dps
 		
 class Broca(Operator):
-	def __init__(self, pp, lvl = 0, pot=-1, skill=-1, mastery = 3, module=-1, module_lvl = 3, targets=1, TrTaTaSkMo=[True,True,True,True,True], buffs=[0,0,0],**kwargs):
-		maxlvl=80
-		lvl1atk = 659  #######including trust
-		maxatk = 842
-		self.atk_interval = 1.2   #### in seconds
-		level = lvl if lvl > 0 and lvl < maxlvl else maxlvl
-		self.base_atk = lvl1atk + (maxatk-lvl1atk) * (level-1) / (maxlvl-1)
-		self.pot = pot if pot in range(1,7) else 1
-		if self.pot > 3: self.base_atk += 28
-		
-		self.skill = skill if skill in [1,2] else 2 ###### check implemented skills
-		self.mastery = mastery if mastery in [0,1,2,3] else 3
-		if level != maxlvl: self.name = f"Broca Lv{level} P{self.pot} S{self.skill}" #####set op name
-		else: self.name = f"Broca P{self.pot} S{self.skill}"
-		if self.mastery == 0: self.name += "L7"
-		elif self.mastery < 3: self.name += f"M{self.mastery}"
-		self.targets = max(1,targets)
-		self.talent1 = TrTaTaSkMo[1]
-		self.skilldmg = TrTaTaSkMo[3]
-		self.moduledmg = TrTaTaSkMo[4]
-		
-		self.module = module if module in [0,1,2] else 1 ##### check valid modules
-		self.module_lvl = module_lvl if module_lvl in [1,2,3] else 3		
-		if level >= maxlvl-30:
-			if self.module == 1:
-				if self.module_lvl == 3: self.base_atk += 50
-				elif self.module_lvl == 2: self.base_atk += 42
-				else: self.base_atk += 30
-				self.name += f" ModX{self.module_lvl}"
-			else: self.name += " no Mod"
-		else: self.module = 0
-		
-		##### keep the ones that apply
-		if self.talent1: self.name += " blocking2+"
-		if self.module == 1 and self.moduledmg: self.name += " vsBlocked"
-		
+	def __init__(self, pp, *args, **kwargs):
+		super().__init__("Broca",pp,[1,2],[1],2,1,1)
+		if self.talent_dmg and self.elite > 0: self.name += " blocking2+"
+		if self.module == 1 and self.module_dmg: self.name += " vsBlocked"
 		if self.targets > 1: self.name += f" {self.targets}targets" ######when op has aoe
-		
-		self.buffs = buffs
-			
-	
+
 	def skill_dps(self, defense, res):
-		dps = 0
-		atkbuff = self.buffs[0]
-		aspd = self.buffs[2]
-		atk_scale = 1
-		
-		#talent/module buffs
-		if self.talent1:
-			atkbuff += 0.12
-			if self.pot > 4: atkbuff += 0.02
-			if self.module == 1:
-				if self.module_lvl == 2: atkbuff += 0.03
-				if self.module_lvl == 3: atkbuff += 0.05
-
-		if self.module == 1 and self.moduledmg:
-			atk_scale = 1.1
-			
-		####the actual skills
-		if self.skill == 1:
-			atkbuff += 0.5 + 0.1 * self.mastery
-			final_atk = self.base_atk * (1+atkbuff) + self.buffs[1]
-
-			hitdmgarts = np.fmax(final_atk *atk_scale *(1-res/100), final_atk * atk_scale * 0.05)
-			
-			dps = hitdmgarts/(self.atk_interval/(1+aspd/100)) *min(3, self.targets)
-		
-		if self.skill == 2:
-			self.atk_interval = 1.98
-			atkbuff += 1.4 + 0.2 * self.mastery
-			if self.mastery > 1: atkbuff -= 0.1
-			final_atk = self.base_atk * (1+atkbuff) + self.buffs[1]
-			hitdmgarts = np.fmax(final_atk * atk_scale *(1-res/100), final_atk* atk_scale * 0.05)
-			
-			dps = hitdmgarts/(self.atk_interval/(1+aspd/100)) * min(3,self.targets)
+		atkbuff = self.talent1_params[0] if self.talent_dmg else 0
+		atk_scale = 1.1 if self.module == 1 and self.module_dmg else 1
+		atkbuff += self.skill_params[0]
+		final_atk = self.atk * (1 + self.buff_atk + atkbuff) + self.buff_atk_flat
+		if self.skill == 2: self.atk_interval = 1.98
+		hitdmgarts = np.fmax(final_atk * atk_scale * (1-res/100), final_atk * atk_scale * 0.05)
+		dps = hitdmgarts/self.atk_interval * self.attack_speed/100 * min(3, self.targets)
 		return dps
 
 class Bryophyta(Operator):
