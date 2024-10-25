a8833b6563e981475481faa18d0b29e0f6d4e4ab
WhoAteMyCQQkie <128926564+WhoAteMyCQQkie@users.noreply.github.com>
2024-10-25 10:35:58 +0200

    Updated Swire Alter

--- damagecalc/damage_formulas.py
+++ damagecalc/damage_formulas.py
@@ -8020,109 +8020,41 @@ class Suzuran(Operator):
 		return dps*(1+fragile)/(1+self.buff_fragile)
 
 class SwireAlt(Operator):
-	def __init__(self, pp, lvl = 0, pot=-1, skill=-1, mastery = 3, module=-1, module_lvl = 3, targets=1, TrTaTaSkMo=[True,True,True,True,True], buffs=[0,0,0],**kwargs):
-		maxlvl=90
-		lvl1atk = 727 #######including trust
-		maxatk = 865
-		self.atk_interval = 1.0   #### in seconds
-		level = lvl if lvl > 0 and lvl < maxlvl else maxlvl
-		self.base_atk = lvl1atk + (maxatk-lvl1atk) * (level-1) / (maxlvl-1)
-		self.pot = pot if pot in range(1,7) else 1
-		if self.pot > 3: self.base_atk += 30
-		
-		self.skill = skill if skill in [1,2,3] else 3 ###### check implemented skills
-		self.mastery = mastery if mastery in [0,1,2,3] else 3
-		if level != maxlvl: self.name = f"SwireAlt Lv{level} P{self.pot} S{self.skill}" #####set op name
-		else: self.name = f"SwireAlt P{self.pot} S{self.skill}"
-		if self.mastery == 0: self.name += "L7"
-		elif self.mastery < 3: self.name += f"M{self.mastery}"
-		self.talent1 = TrTaTaSkMo[1]
-		self.skilldmg = TrTaTaSkMo[3]
-		self.moduledmg = TrTaTaSkMo[4]
-		
-		self.module = module if module in [0,1,2] else 1 ##### check valid modules
-		self.module_lvl = module_lvl if module_lvl in [1,2,3] else 3		
-		if level >= maxlvl-30:
-			if self.module == 1:
-				if self.module_lvl == 3: self.base_atk += 40
-				elif self.module_lvl == 2: self.base_atk += 35
-				else: self.base_atk += 25
-				self.name += f" ModX{self.module_lvl}"
-			elif self.module == 2:
-				if self.module_lvl == 3: self.base_atk += 81
-				elif self.module_lvl == 2: self.base_atk += 71
-				else: self.base_atk += 57
-				self.name += f" ModY{self.module_lvl}"
-			else: self.name += " no Mod"
-		else: self.module = 0
-		
-		  ##### keep the ones that apply
-		self.stacks = 9 if self.pot > 4 else 8
-		
-		if not self.talent1:
-			self.name += " noStacks"
-			self.stacks = 0
-		else: self.name += f" {self.stacks}Stacks"
-		
-		if self.skilldmg and self.skill == 2: self.name += " 2HitBottles"
-		if not self.skilldmg and self.skill == 1: self.name += " heals>attacks"
-		if self.module == 2 and self.talent1: self.name += " maxModStacks"
-		 ######when op has aoe
-		
-		self.buffs = buffs
-				
-	def skill_dps(self, defense, res):
-		dps = 0
-		atkbuff = self.buffs[0]
-		aspd = self.buffs[2]
-		atk_scale = 1
-		
-		#talent/module buffs
-		if self.talent1:
-			atkbuff += self.stacks * 0.04
-			if self.module == 1 and self.module_lvl > 1: atkbuff += self.stacks * 0.01
-			if self.module == 2: atkbuff += 0.2
-			
-		####the actual skills
-		if self.skill == 1:
-			final_atk = self.base_atk * (1+atkbuff) + self.buffs[1]
-			atkcycle= (self.atk_interval/(1+aspd/100))
-			hitdmg = np.fmax(final_atk - defense, final_atk * 0.05)
-			dps = hitdmg/(self.atk_interval/(1+aspd/100))
-			if not self.skilldmg: dps = dps * (3/atkcycle-1) /(3/atkcycle)
-		
-		if self.skill == 2:
-			skill_scale = 1.7 + 0.1 * self.mastery
-			
-			final_atk = self.base_atk * (1+atkbuff) + self.buffs[1]
-			
-			hitdmg = np.fmax(final_atk - defense, final_atk * 0.05)
-			skilldmg = np.fmax(final_atk * skill_scale - defense, final_atk * skill_scale * 0.05)
-			if self.skilldmg: skilldmg *= 2
-			
-			atkcycle= (self.atk_interval/(1+aspd/100))
-			
-			dps = hitdmg/(self.atk_interval/(1+aspd/100))
-			dps = dps * (3/atkcycle-1) /(3/atkcycle) + skilldmg / 3
-		
+	def __init__(self, pp, *args, **kwargs):
+		super().__init__("SwireAlter",pp,[1,2,3],[1,2],3,1,1)
+		if not self.talent_dmg and self.elite > 0: self.name += " noStacks"
+		elif self.elite > 0: self.name += f" {self.talent1_params[3]}Stacks"
+		if self.skill_dmg and self.skill == 2: self.name += " 2HitBottles"
+		if not self.skill_dmg and self.skill == 1: self.name += " heals>attacks"
+		if self.module == 2 and self.talent_dmg: self.name += " maxModStacks"
 		if self.skill == 3:
-			final_atk = self.base_atk * (1+atkbuff) + self.buffs[1]
-			hitdmg = np.fmax(final_atk - defense, final_atk * 0.05)
-			dps = hitdmg/(self.atk_interval/(1+aspd/100)) * 2
-		return dps
-
-	def get_name(self):
-		if self.skill == 3:
-
-			atk = self.stacks * 0.04
-			if self.module == 1 and self.module_lvl > 1: atk += self.stacks * 0.01
+			atk = self.talent1_params[3] * self.talent1_params[2]
 			if self.module == 2: atk += 0.2
-			
-			skill_scale = 1.2 + 0.1 * self.mastery
-			final_atk = self.base_atk * (1+self.buffs[0] + atk) + self.buffs[1]
-			nukedmg = final_atk * 10 * skill_scale * (1+self.buffs[3])
+			final_atk = self.atk * (1+self.buff_atk + atk) + self.buff_atk_flat
+			nukedmg = final_atk * 10 * self.skill_params[0] * (1+self.buff_fragile)
 			self.name += f" 10Coins:{int(nukedmg)}"
-		return self.name
+
+	def skill_dps(self, defense, res):
+		atkbuff = 0
+		if self.talent_dmg and self.elite > 0:
+			atkbuff = self.talent1_params[3] * self.talent1_params[2]
+			if self.module == 2: atkbuff += 0.2
+
+		atkcycle = (self.atk_interval/(self.attack_speed/100))
+		final_atk = self.atk * (1 + atkbuff + self.buff_atk) + self.buff_atk_flat
+		hitdmg = np.fmax(final_atk - defense, final_atk * 0.05)
+		if self.skill == 1:
+			dps = hitdmg/(self.atk_interval/(self.attack_speed/100))
+			if not self.skill_dmg: dps = dps * (3/atkcycle-1) /(3/atkcycle)
+		if self.skill == 2:
+			skill_scale = self.skill_params[1]
+			skilldmg = np.fmax(final_atk * skill_scale - defense, final_atk * skill_scale * 0.05)
+			if self.skill_dmg: skilldmg *= 2
+			dps = hitdmg/(self.atk_interval/(self.attack_speed/100))
+			dps = dps * (3/atkcycle-1) /(3/atkcycle) + skilldmg / 3
+		if self.skill == 3:
+			dps = hitdmg/(self.atk_interval/(self.attack_speed/100)) * 2
+		return dps
 
 class Tachanka(Operator):
 	def __init__(self, pp, *args, **kwargs):
