1ef4eb0bd5e2b67bb4a0318e098a43a3e74a1fce
WhoAteMyCQQkie <128926564+WhoAteMyCQQkie@users.noreply.github.com>
2024-10-29 20:17:07 +0100

    Updated Grani

--- damagecalc/damage_formulas.py
+++ damagecalc/damage_formulas.py
@@ -3203,42 +3203,16 @@ class Goldenglow(Operator):
 		return dps
 
 class Grani(Operator):
-	def __init__(self, pp, lvl = 0, pot=-1, skill=-1, mastery = 3, module=-1, module_lvl = 3, targets=1, TrTaTaSkMo=[True,True,True,True,True], buffs=[0,0,0],**kwargs):
-		maxlvl=80
-		lvl1atk = 463  #######including trust
-		maxatk = 552
-		self.atk_interval = 1.0   #### in seconds
-		level = lvl if lvl > 0 and lvl < maxlvl else maxlvl
-		self.base_atk = lvl1atk + (maxatk-lvl1atk) * (level-1) / (maxlvl-1)
-		self.pot = pot if pot in range(1,7) else 6
-		
-		self.skill = skill if skill in [2] else 2 ###### check implemented skills
-		self.mastery = mastery if mastery in [0,1,2,3] else 3
-		if level != maxlvl: self.name = f"Grani Lv{level} P{self.pot} S{self.skill}" #####set op name
-		else: self.name = f"Grani P{self.pot} S{self.skill}"
-		if self.mastery == 0: self.name += "L7"
-		elif self.mastery < 3: self.name += f"M{self.mastery}"
-		
-		self.module = module if module in [0,1] else 1 ##### check valid modules
-		self.module_lvl = module_lvl if module_lvl in [1,2,3] else 3		
-		if level >= maxlvl-30:
-			if self.module == 2:
-				self.name += f" ModX{self.module_lvl}"
-			else: self.name += " no Mod"
-		else: self.module = 0
-		self.targets = max(1,targets)
-		self.buffs = buffs
-		if self.targets > 1: self.name += f" {self.targets}targets"
+	def __init__(self, pp, *args, **kwargs):
+		super().__init__("Grani",pp,[1,2],[1],2,6,1)
+		if self.targets > 1 and self.skill == 2: self.name += f" {self.targets}targets"
 	
 	def skill_dps(self, defense, res):
-		dps = 0
-		atkbuff = self.buffs[0]
-		aspd = self.buffs[2]
-
-		atkbuff += 0.8 if self.mastery == 3 else 0.6 + 0.06 * self.mastery
-		final_atk = self.base_atk * (1+atkbuff) + self.buffs[1]
+		targets = 2 if self.skill == 2 else 1
+		atkbuff = self.skill_params[0] if self.skill == 2 else 0
+		final_atk = self.atk * (1 + atkbuff + self.buff_atk) + self.buff_atk_flat
 		hitdmg = np.fmax(final_atk - defense, final_atk * 0.05)
-		dps = hitdmg/(self.atk_interval/(1+aspd/100)) * min(self.targets,2)
+		dps = hitdmg/self.atk_interval * self.attack_speed/100 * min(self.targets,targets)
 		return dps
 
 class GreyThroat(Operator):
