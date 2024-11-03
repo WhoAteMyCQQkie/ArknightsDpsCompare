7ee524fcc2f458d9e78bbcd52a6e670eb15a3e8d
WhoAteMyCQQkie <128926564+WhoAteMyCQQkie@users.noreply.github.com>
2024-11-03 11:57:51 +0100

    Updated Firewhistle

--- damagecalc/damage_formulas.py
+++ damagecalc/damage_formulas.py
@@ -2640,85 +2640,29 @@ class Fiammetta(Operator):
 		return dps
 	
 class Firewhistle(Operator):
-	def __init__(self, pp, lvl = 0, pot=-1, skill=-1, mastery = 3, module=-1, module_lvl = 3, targets=1, TrTaTaSkMo=[True,True,True,True,True], buffs=[0,0,0],**kwargs):
-		maxlvl=80
-		lvl1atk = 782  #######including trust
-		maxatk = 932
-		self.atk_interval = 2.8   #### in seconds
-		level = lvl if lvl > 0 and lvl < maxlvl else maxlvl
-		self.base_atk = lvl1atk + (maxatk-lvl1atk) * (level-1) / (maxlvl-1)
-		self.pot = pot if pot in range(1,7) else 1
-		if self.pot > 3: self.base_atk += 32
-		
-		self.skill = skill if skill in [1,2] else 2 ###### check implemented skills
-		self.mastery = mastery if mastery in [0,1,2,3] else 3
-		if level != maxlvl: self.name = f"Firewhistle Lv{level} P{self.pot} S{self.skill}" #####set op name
-		else: self.name = f"Firewhistle P{self.pot} S{self.skill}"
-		if self.mastery == 0: self.name += "L7"
-		elif self.mastery < 3: self.name += f"M{self.mastery}"
-		self.targets = max(1,targets)
-		self.trait = TrTaTaSkMo[0]
-		self.talent1 = TrTaTaSkMo[1]
-		self.talent2 = TrTaTaSkMo[2]
-		self.skilldmg = TrTaTaSkMo[3]
-		self.moduledmg = TrTaTaSkMo[4]
-		
-		self.module = module if module in [0,1] else 1 ##### check valid modules
-		self.module_lvl = module_lvl if module_lvl in [1,2,3] else 3		
-		if level >= maxlvl-30:
-			if self.module == 1:
-				if self.module_lvl == 3: self.base_atk += 58
-				elif self.module_lvl == 2: self.base_atk += 52
-				else: self.base_atk += 42
-				self.name += f" ModX{self.module_lvl}"
-			else: self.name += " no Mod"
-		else: self.module = 0
-		
-		if not self.talent1: self.name += " meelee"
-		if self.moduledmg and self.module == 1: self.name += " vsBlocked"
-		
-		if self.targets > 1: self.name += f" {self.targets}targets" ######when op has aoe
-		
-		self.buffs = buffs
-			
-	
-	def skill_dps(self, defense, res):
-		dps = 0
-		atkbuff = self.buffs[0]
-		aspd = self.buffs[2]
-		atk_scale = 1
-		
-		#talent/module buffs
-		if self.module == 1 and self.moduledmg:
-			atk_scale = 1.1
+	def __init__(self, pp, *args, **kwargs):
+		super().__init__("Firewhistle",pp,[1,2],[1],2,1,1)
+		if not self.talent_dmg and self.elite > 0: self.name += " meelee"
+		if self.module_dmg and self.module == 1: self.name += " vsBlocked"
+		if self.targets > 1: self.name += f" {self.targets}targets"
 
-		if self.talent1:
-			atkbuff += 0.12
-			if self.pot > 4: atkbuff += 0.02
-			if self.module == 1:
-				atkbuff += 0.04 * (self.module_lvl - 1)
-			
-		####the actual skills
+	def skill_dps(self, defense, res):
+		atkbuff = self.talent1_params[0] if self.talent_dmg else 0
+		atk_scale = 1.1 if self.module == 1 and self.module_dmg else 1
+		final_atk = self.atk * (1 + atkbuff + self.buff_atk) + self.buff_atk_flat
 		if self.skill == 1:
-			skill_scale = 1.5 + 0.1 * self.mastery
-			if self.mastery > 1: skill_scale += 0.05
-			fire_scale = 0.5 if self.mastery == 3 else 0.4
-			fire_scale *= 4
-			
-			final_atk = self.base_atk * (1+atkbuff) + self.buffs[1]
-			
+			skill_scale = self.skill_params[2]
+			fire_scale = self.skill_params[1] * self.skill_params[0]
 			hitdmg = np.fmax(final_atk * atk_scale - defense, final_atk * atk_scale * 0.05)
 			hitdmgskill = np.fmax(final_atk * atk_scale * skill_scale - defense, final_atk * atk_scale * skill_scale * 0.05)
 			hitdmgarts = np.fmax(final_atk * atk_scale * fire_scale * (1-res/100), final_atk * 0.05)
 			avgdmg = 3/4 * self.targets * hitdmg + 1/4 * hitdmgskill * self.targets + hitdmgarts / 4
-			dps = avgdmg/(self.atk_interval/(1+aspd/100))
-		
+			dps = avgdmg/self.atk_interval * self.attack_speed/100
 		if self.skill == 2:
-			skill_scale = 0.65 if self.mastery == 0 else 0.7 + 0.05 * self.mastery
-			final_atk = self.base_atk * (1+atkbuff) + self.buffs[1]
+			skill_scale = self.skill_params[1]
 			hitdmg = np.fmax(final_atk * atk_scale - defense, final_atk * atk_scale * 0.05)
 			hitdmgarts = np.fmax(final_atk * atk_scale * skill_scale * (1-res/100), final_atk * atk_scale * skill_scale * 0.05)
-			dps = hitdmg/(self.atk_interval/(1+aspd/100)) + hitdmgarts
+			dps = hitdmg/self.atk_interval * self.attack_speed/100 + hitdmgarts
 			dps = dps * self.targets
 		return dps
 
