affbff8a30e28aace35fd68bfe6a47f42e31b246
WhoAteMyCQQkie <128926564+WhoAteMyCQQkie@users.noreply.github.com>
2024-11-05 17:17:07 +0100

    Updated Pozemka

--- damagecalc/damage_formulas.py
+++ damagecalc/damage_formulas.py
@@ -5720,90 +5720,55 @@ class Popukar(Operator):
 		return dps
 
 class Pozemka(Operator):
-	def __init__(self, pp, lvl = 0, pot=-1, skill=-1, mastery = 3, module=-1, module_lvl = 3, targets=1, TrTaTaSkMo=[True,True,True,True,True], buffs=[0,0,0],**kwargs):
-		maxlvl=90
-		lvl1atk = 744  #######including trust
-		maxatk = 946
-		self.atk_interval = 1.6   #### in seconds
-		level = lvl if lvl > 0 and lvl < maxlvl else maxlvl
-		self.base_atk = lvl1atk + (maxatk-lvl1atk) * (level-1) / (maxlvl-1)
-		self.typewriter_atk = 666 + 200 * (level-1) / (maxlvl-1)
-		self.pot = pot if pot in range(1,7) else 1
-		if self.pot > 3: self.base_atk += 30
-		
-		self.skill = skill if skill in [3] else 3 ###### check implemented skills
-		self.mastery = mastery if mastery in [0,1,2,3] else 3
-		if level != maxlvl: self.name = f"Pozemka Lv{level} P{self.pot} S{self.skill}" #####set op name
-		else: self.name = f"Pozemka P{self.pot} S{self.skill}"
-		if self.mastery == 0: self.name += "L7"
-		elif self.mastery < 3: self.name += f"M{self.mastery}"
-		
-		self.talent1 = TrTaTaSkMo[1]
-		self.talent2 = TrTaTaSkMo[2]
-		self.skilldmg = TrTaTaSkMo[3]
-		self.moduledmg = TrTaTaSkMo[4]
-		
-		self.module = module if module in [0,2] else 2 ##### check valid modules
-		self.module_lvl = module_lvl if module_lvl in [1,2,3] else 3		
-		if level >= maxlvl-30:
-			if self.module == 2:
-				if self.module_lvl == 3:
-					self.base_atk += 75
-					self.typewriter_atk += 70				
-				elif self.module_lvl == 2:
-					self.base_atk += 65
-					self.typewriter_atk += 50
-				else: self.base_atk += 55
-				self.name += f" ModY{self.module_lvl}"
-			else: self.name += " no Mod"
-		else: self.module = 0
-		
-
-		if not self.talent1:
+	def __init__(self, pp, *args, **kwargs):
+		super().__init__("Pozemka",pp,[1,3],[2],3,1,2)
+		if self.module == 2 and self.module_lvl > 1: self.drone_atk += 10 + 20 * self.module_lvl
+		if not self.talent_dmg and self.elite > 0:
 			self.name += " w/o Typewriter"
-			self.talent2 = False
-		elif not self.talent2: self.name += " TW separate"
-		else: self.name += " AdjacentTW"
-		
+			self.talent2_dmg = False
+		elif not self.talent2_dmg and self.elite > 1: self.name += " TW separate"
+		self.pot = pp.pot
 		if self.skill == 3:
-			self.moduledmg = self.moduledmg and self.skilldmg
-		if self.module == 2 and self.moduledmg: self.name += " DirectFront"
-		
-		self.buffs = buffs
-	
+			self.module_dmg = self.module_dmg and self.skill_dmg
+		if self.module == 2 and self.module_dmg: self.name += " DirectFront"
+
 	def skill_dps(self, defense, res):
-		dps = 0
-		atkbuff = self.buffs[0]
-		aspd = self.buffs[2]
-		atk_scale = 1
-		
-		#talent/module buffs
 		defshred = 0
-		if self.talent1:
-			if self.talent2:
+		if self.talent_dmg:
+			if self.talent2_dmg:
 				defshred = 0.25 if self.pot > 4 else 0.23
 			else:
 				defshred = 0.2 if self.pot > 4 else 0.18
 		newdef = defense * (1-defshred)
-		if self.moduledmg and self.module == 2:
-			atk_scale = 1.05
-			
+		atk_scale = 1.05 if self.module_dmg and self.module == 2 else 1
+
+		if self.skill == 1:
+			final_atk = self.atk * (1 + self.buff_atk + self.skill_params[0]) + self.buff_atk_flat
+			rate = self.skill_params[1]
+			skill_scale = self.skill_params[2]
+			hitdmg = np.fmax(final_atk * atk_scale - newdef, final_atk * atk_scale * 0.05)
+			hitdmg2 = np.fmax(final_atk * atk_scale * skill_scale - newdef, final_atk * atk_scale * skill_scale * 0.05)
+			avghit = rate * hitdmg2 + (1 - rate) * hitdmg
+			dps = avghit/self.atk_interval * self.attack_speed/100
+			if self.talent_dmg and self.elite > 0:
+				final_atk2 = self.drone_atk * (1 + self.skill_params[0])
+				hitdmg = np.fmax(final_atk2 * atk_scale - newdef, final_atk2 * atk_scale * 0.05)
+				hitdmg2 = np.fmax(final_atk2 * atk_scale * skill_scale - newdef, final_atk2 * atk_scale * skill_scale * 0.05)
+				avghit = rate * hitdmg2 + (1 - rate) * hitdmg
+				dps += avghit/self.drone_atk_interval
+
 		if self.skill == 3:
 			self.atk_interval = 1
-			skill_scale = 1.7 + 0.1 * self.mastery
-			skill_scale2 = 2.1 + 0.15 * self.mastery
-			if self.mastery < 2: skill_scale2 += 0.05
-
-			final_atk = self.base_atk * (1+atkbuff) + self.buffs[1]
+			skill_scale = self.skill_params[1]
+			skill_scale2 = self.skill_params[2]
+			final_atk = self.atk * (1 + self.buff_atk) + self.buff_atk_flat
 			hitdmg = np.fmax(final_atk * atk_scale * skill_scale - newdef, final_atk * atk_scale * skill_scale * 0.05)
-			if self.moduledmg or self.skilldmg:
+			if self.module_dmg or self.skill_dmg:
 				hitdmg = np.fmax(final_atk * atk_scale * skill_scale2 - newdef, final_atk * atk_scale * skill_scale2 * 0.05)	
-			
 			hitdmgTW = 0
-			if self.talent1:
-				hitdmgTW = np.fmax(self.typewriter_atk * skill_scale2 - newdef, self.typewriter_atk * skill_scale2 * 0.05)
-			dps = hitdmg/(self.atk_interval/(1+aspd/100)) + hitdmgTW
-
+			if self.talent_dmg:
+				hitdmgTW = np.fmax(self.drone_atk * skill_scale2 - newdef, self.drone_atk * skill_scale2 * 0.05)
+			dps = hitdmg/self.atk_interval * self.attack_speed/100 + hitdmgTW
 		return dps
 
 class ProjektRed(Operator):
