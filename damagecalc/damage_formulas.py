dc658482029a4d161b1f0d10f96400ad96c0e3ec
WhoAteMyCQQkie <128926564+WhoAteMyCQQkie@users.noreply.github.com>
2024-11-09 16:43:09 +0100

    Updated Utage

--- damagecalc/damage_formulas.py
+++ damagecalc/damage_formulas.py
@@ -6859,56 +6859,20 @@ class Ulpianus(Operator):
 		return dps
 
 class Utage(Operator):
-	def __init__(self, pp, lvl = 0, pot=-1, skill=-1, mastery = 3, module=-1, module_lvl = 3, targets=1, TrTaTaSkMo=[True,True,True,True,True], buffs=[0,0,0],**kwargs):
-		maxlvl=70
-		lvl1atk = 605  #######including trust
-		maxatk = 723
-		self.atk_interval = 1.2   #### in seconds
-		level = lvl if lvl > 0 and lvl < maxlvl else maxlvl
-		self.base_atk = lvl1atk + (maxatk-lvl1atk) * (level-1) / (maxlvl-1)
-		self.pot = pot if pot in range(1,7) else 6
-		if self.pot > 3: self.base_atk += 23
-		
-		self.skill = skill if skill in [2] else 2 ###### check implemented skills
-		self.mastery = mastery if mastery in [0,1,2,3] else 3
-		if level != maxlvl: self.name = f"Utage Lv{level} P{self.pot} S{self.skill}" #####set op name
-		else: self.name = f"Utage P{self.pot} S{self.skill}"
-		if self.mastery == 0: self.name += "L7"
-		elif self.mastery < 3: self.name += f"M{self.mastery}"
-		self.talent1 = TrTaTaSkMo[1]
-		self.skilldmg = TrTaTaSkMo[3]
-		self.moduledmg = TrTaTaSkMo[4]
-		
-		self.module = module if module in [0,1] else 1 ##### check valid modules
-		self.module_lvl = module_lvl if module_lvl in [1,2,3] else 3		
-		if level >= maxlvl-30:
-			if self.module == 1:
-				if self.module_lvl == 3: self.base_atk += 60
-				elif self.module_lvl == 2: self.base_atk += 55
-				else: self.base_atk += 45
-				self.name += f" ModX{self.module_lvl}"
-			else: self.name += " no Mod"
-		else: self.module = 0
-		
-		if self.talent1: self.name += " lowHP"
-		else: self.name += " fullHP"
-		
-		self.buffs = buffs
-				
-	def skill_dps(self, defense, res):
-		dps = 0
-		atkbuff = self.buffs[0]
-		aspd = self.buffs[2]
-		
-		if self.talent1:
-			aspd += 100
-			if self.module == 1 and self.module_lvl > 1: atkbuff += 0.02 + 0.01 * self.module_lvl
-
+	def __init__(self, pp, *args, **kwargs):
+		super().__init__("Utage",pp,[1,2],[1],2,6,1)
 		if self.skill == 2:
-			atkbuff += 0.8 + 0.1 * self.mastery
-			final_atk = self.base_atk * (1+atkbuff) + self.buffs[1]
+			if self.talent_dmg: self.name += " lowHP"
+			else: self.name += " fullHP"
+
+	def skill_dps(self, defense, res):
+		if self.skill == 1: return 0 * res
+		aspd = self.talent1_params[0] if self.talent_dmg else 0
+		atkbuff = 0.01 + 0.01 * self.module_lvl if self.module == 1 and self.module_lvl > 1 and self.talent_dmg else 0
+		if self.skill == 2:
+			final_atk = self.atk * (1 + atkbuff + self.buff_atk + self.skill_params[0]) + self.buff_atk_flat
 			hitdmg = np.fmax(final_atk * (1-res/100), final_atk * 0.05)
-			dps = hitdmg/(self.atk_interval/(1+aspd/100))
+			dps = hitdmg/self.atk_interval * (self.attack_speed+aspd)/100
 		return dps
 
 class Vanilla(Operator):
