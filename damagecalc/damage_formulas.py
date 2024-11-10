2475ec69ae23cdc0187ddbb5ebaca5f261a8b3d5
WhoAteMyCQQkie <128926564+WhoAteMyCQQkie@users.noreply.github.com>
2024-11-10 18:02:14 +0100

    Updated Hellagur

--- damagecalc/damage_formulas.py
+++ damagecalc/damage_formulas.py
@@ -2983,87 +2983,29 @@ class Haze(Operator):
 		return dps
 	
 class Hellagur(Operator):
-	def __init__(self, pp, lvl = 0, pot=-1, skill=-1, mastery = 3, module=-1, module_lvl = 3, targets=1, TrTaTaSkMo=[True,True,True,True,True], buffs=[0,0,0],**kwargs):
-		maxlvl=90
-		lvl1atk = 702  #######including trust
-		maxatk = 832
-		self.atk_interval = 1.2   #### in seconds
-		level = lvl if lvl > 0 and lvl < maxlvl else maxlvl
-		self.base_atk = lvl1atk + (maxatk-lvl1atk) * (level-1) / (maxlvl-1)
-		self.pot = pot if pot in range(1,7) else 1
-		if self.pot > 3: self.base_atk += 26
-		
-		self.skill = skill if skill in [1,2,3] else 3 ###### check implemented skills
-		self.mastery = mastery if mastery in [0,1,2,3] else 3
-		if level != maxlvl: self.name = f"Hellagur Lv{level} P{self.pot} S{self.skill}" #####set op name
-		else: self.name = f"Hellagur P{self.pot} S{self.skill}"
-		if self.mastery == 0: self.name += "L7"
-		elif self.mastery < 3: self.name += f"M{self.mastery}"
-		self.targets = max(1,targets)
-		self.trait = TrTaTaSkMo[0]
-		self.talent1 = TrTaTaSkMo[1]
-		self.skilldmg = TrTaTaSkMo[3]
-		self.moduledmg = TrTaTaSkMo[4]
-		
-		self.module = module if module in [0,1,2] else 1 ##### check valid modules
-		self.module_lvl = module_lvl if module_lvl in [1,2,3] else 3		
-		if level >= maxlvl-30:
-			if self.module == 1:
-				if self.module_lvl == 3: self.base_atk += 90
-				elif self.module_lvl == 2: self.base_atk += 75
-				else: self.base_atk += 55
-				self.name += f" ModX{self.module_lvl}"
-			elif self.module == 2:
-				if self.module_lvl == 3: self.base_atk += 90
-				elif self.module_lvl == 2: self.base_atk += 75
-				else: self.base_atk += 55
-				self.name += f" ModY{self.module_lvl}"
-			else: self.name += " no Mod"
-		else: self.module = 0
-		
-		if self.talent1: self.name += " lowHP"
+	def __init__(self, pp, *args, **kwargs):
+		super().__init__("Hellagur",pp,[1,2,3],[1,2],3,1,1)
+		if self.talent_dmg: self.name += " lowHP"
 		else: self.name += " fullHP"
-		
 		if self.targets > 1 and self.skill == 3: self.name += f" {self.targets}targets" ######when op has aoe
-		
-		self.buffs = buffs
-			
-	
+
 	def skill_dps(self, defense, res):
-		dps = 0
-		atkbuff = self.buffs[0]
-		aspd = self.buffs[2]
-		atk_scale = 1
-		
-		#talent/module buffs
-		if self.talent1:
-			aspd += 100
-			if self.module == 1 and self.module_lvl > 1:
-				aspd += 10 * self.module_lvl
-			
-		####the actual skills
+		aspd = max(self.talent1_params) if self.talent_dmg else 0
+		atkbuff = self.skill_params[0] if self.skill != 1 else 0
+		final_atk = self.atk * (1 + self.buff_atk + atkbuff) + self.buff_atk_flat
 		if self.skill == 1:
-			skill_scale = 1.45 + 0.1 * self.mastery
-			
-			final_atk = self.base_atk * (1+atkbuff) + self.buffs[1]
-			
-			hitdmg = np.fmax(final_atk * atk_scale - defense, final_atk * atk_scale * 0.05)
-			skillhitdmg = np.fmax(final_atk * atk_scale *skill_scale - defense, final_atk* atk_scale * skill_scale * 0.05) * 2
-			sp_cost = 2 if self.mastery == 3 else 3
-			avgphys = (sp_cost * hitdmg + skillhitdmg) / (sp_cost + 1)
-			dps = avgphys/(self.atk_interval/(1+aspd/100))
-			
-		if self.skill == 2:
-			atkbuff += 0.5 + 0.1 * self.mastery
-			final_atk = self.base_atk * (1+atkbuff) + self.buffs[1]	
-			hitdmg = np.fmax(final_atk - defense, final_atk * 0.05) * 2
-			dps = hitdmg/(self.atk_interval/(1+aspd/100))
-		
-		if self.skill == 3:
-			atkbuff += 0.7 + 0.1 * self.mastery
-			final_atk = self.base_atk * (1+atkbuff) + self.buffs[1]	
+			skill_scale = self.skill_params[0]
 			hitdmg = np.fmax(final_atk - defense, final_atk * 0.05)
-			dps = hitdmg/(self.atk_interval/(1+aspd/100)) * min(self.targets, 3)
+			skillhitdmg = np.fmax(final_atk * skill_scale - defense, final_atk * skill_scale * 0.05) * 2
+			sp_cost = self.skill_cost
+			avgphys = (sp_cost * hitdmg + skillhitdmg) / (sp_cost + 1)
+			dps = avgphys/self.atk_interval * (self.attack_speed+aspd)/100
+		if self.skill == 2:
+			hitdmg = np.fmax(final_atk - defense, final_atk * 0.05) * 2
+			dps = hitdmg/self.atk_interval * (self.attack_speed+aspd)/100
+		if self.skill == 3:
+			hitdmg = np.fmax(final_atk - defense, final_atk * 0.05)
+			dps = hitdmg/self.atk_interval * (self.attack_speed+aspd)/100 * min(self.targets, 3)
 		return dps
 
 class Hibiscus(Operator):
