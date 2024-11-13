fdc76309376b5fef70bb60af4a9d573a556659af
WhoAteMyCQQkie <128926564+WhoAteMyCQQkie@users.noreply.github.com>
2024-11-13 17:06:36 +0100

    Updated Weedy

--- damagecalc/damage_formulas.py
+++ damagecalc/damage_formulas.py
@@ -5522,7 +5522,7 @@ class Ray(Operator):
 	
 	def total_dmg(self, defense, res):
 		if self.skill == 3:
-			return(self.skill_dps(defense,res) * 8 * (self.atk_interval/(1+self.buffs[2]/100)))
+			return(self.skill_dps(defense,res) * 8 * (self.atk_interval/(self.attack_speed/100)))
 		else:
 			return(self.skill_dps(defense,res))
 	
@@ -6304,7 +6304,7 @@ class TerraResearchCommission(Operator):
 		return dps
 
 class Thorns(Operator):
-	def __init__(self, pp, lvl = 0, pot=-1, skill=-1, mastery = 3, module=-1, module_lvl = 3, targets=1, TrTaTaSkMo=[True,True,True,True,True], buffs=[0,0,0],**kwargs):
+	def __init__(self, pp, *args, **kwargs):
 		super().__init__("Thorns",pp,[1,2,3],[1],3,1,1)
 		if self.skill == 1 and not self.trait_dmg: self.name += "rangedAtk"   ##### keep the ones that apply
 		if self.talent_dmg: self.name += " vsRanged"
@@ -7147,85 +7147,41 @@ class Warmy(Operator):
 		return dps
 
 class Weedy(Operator): #TODO add weight prompt and actually calc the dmg for s3
-	def __init__(self, pp, lvl = 0, pot=-1, skill=-1, mastery = 3, module=-1, module_lvl = 3, targets=1, TrTaTaSkMo=[True,True,True,True,True], buffs=[0,0,0,0,0],**kwargs):
-		maxlvl=90
-		lvl1atk = 593  #######including trust
-		maxatk = 722
-		self.atk_interval = 1.2   #### in seconds
-		level = lvl if lvl > 0 and lvl < maxlvl else maxlvl
-		self.base_atk = lvl1atk + (maxatk-lvl1atk) * (level-1) / (maxlvl-1)
-		self.pot = pot if pot in range(1,7) else 1
-		
-		
-		self.skill = skill if skill in [1,2] else 2 ###### check implemented skills
-		self.mastery = mastery if mastery in [0,1,2,3] else 3
-		if level != maxlvl: self.name = f"Weedy Lv{level} P{self.pot} S{self.skill}" #####set op name
-		else: self.name = f"Weedy P{self.pot} S{self.skill}"
-		if self.mastery == 0: self.name += "L7"
-		elif self.mastery < 3: self.name += f"M{self.mastery}"
-		self.targets = max(1,targets)
-		self.skilldmg = TrTaTaSkMo[3]
-		self.moduledmg = TrTaTaSkMo[4]
-		
-		self.module = module if module in [0,1,2] else 1 ##### check valid modules
-		self.module_lvl = module_lvl if module_lvl in [1,2,3] else 3		
-		if level >= maxlvl-30:
-			if self.module == 1:
-				if self.module_lvl == 3: self.base_atk += 72
-				elif self.module_lvl == 2: self.base_atk += 64
-				else: self.base_atk += 50
-				self.name += f" ModX{self.module_lvl}"
-			elif self.module == 2:
-				if self.module_lvl == 3: self.base_atk += 68
-				elif self.module_lvl == 2: self.base_atk += 60
-				else: self.base_atk += 46
-				self.name += f" ModY{self.module_lvl}"
-			else: self.name += " no Mod"
-		else: self.module = 0
-		
-		if self.module == 1 and self.moduledmg and self.module_lvl > 1: self.name += " nextToCannon"
-		
+	def __init__(self, pp, *args, **kwargs):
+		super().__init__("Weedy",pp,[1,2],[1,2],2,1,1)
+		if self.talent_dmg and self.elite > 0 and not (self.module == 1 and self.module_dmg and self.module_lvl > 1): self.name += " +cannon"
+		if self.module == 1 and self.module_dmg and self.module_lvl > 1 and self.talent_dmg: self.name += " nextToCannon"
 		if self.targets > 1: self.name += f" {self.targets}targets" ######when op has aoe
-		
-		self.buffs = buffs
-	
+
 	def skill_dps(self, defense, res):
-		dps = 0
-		atkbuff = self.buffs[0]
-		aspd = self.buffs[2]
-		atk_scale = 1
-		
-		#talent/module buffs
-		if self.pot > 3: aspd += 8
-		if self.module == 1 and self.moduledmg:
+		atkbuff = 0
+		if self.module == 1 and self.module_dmg:
 			if self.module_lvl == 2: atkbuff += 0.15
 			if self.module_lvl == 3: atkbuff += 0.2
-			
-		####the actual skills
+
 		if self.skill == 1:
-			skill_scale = 1.35 + 0.05 * self.mastery
-			sp_cost = 6 if self.mastery == 0 else 5
-			
-			final_atk = self.base_atk * (1+atkbuff) + self.buffs[1]
-			hitdmg = np.fmax(final_atk * atk_scale - defense, final_atk * atk_scale * 0.05)
-			skilldmg = np.fmax(final_atk * atk_scale * skill_scale - defense, final_atk* atk_scale * skill_scale * 0.05)
-			
-			sp_cost = sp_cost + 1.2 #sp lockout
-			atkcycle = self.atk_interval/(1+aspd/100)
+			skill_scale = self.skill_params[0]
+			sp_cost = self.skill_cost/(1+ self.sp_boost) + 1.2
+			final_atk = self.atk * (1 + self.buff_atk + atkbuff) + self.buff_atk_flat
+			hitdmg = np.fmax(final_atk - defense, final_atk * 0.05)
+			skilldmg = np.fmax(final_atk * skill_scale - defense, final_atk * skill_scale * 0.05)
+			atkcycle = self.atk_interval/(self.attack_speed/100)
 			atks_per_skillactivation = sp_cost / atkcycle
 			avghit = skilldmg
 			if atks_per_skillactivation > 1:
-				avghit = (skilldmg + (atks_per_skillactivation - 1) * hitdmg) / atks_per_skillactivation	
-				
-			dps = avghit/(self.atk_interval/(1+aspd/100)) * self.targets
-		if self.skill == 2:
-			atkbuff += 1.7 + 0.1 * self.mastery
-			self.atk_interval = 1.2 * 3.2
+				avghit = (skilldmg + int(atks_per_skillactivation) * hitdmg) / (int(atks_per_skillactivation)+1)
+			dps = avghit/(self.atk_interval/(self.attack_speed/100)) * self.targets
 
-			final_atk = self.base_atk * (1+atkbuff) + self.buffs[1]
-			
+		if self.skill == 2:
+			atkbuff += self.skill_params[0]
+			final_atk = self.atk * (1 + self.buff_atk + atkbuff) + self.buff_atk_flat
 			hitdmg = np.fmax(final_atk - defense, final_atk * 0.05)
-			dps = hitdmg/(self.atk_interval/(1+aspd/100)) * min(self.targets, 2)
+			dps = hitdmg/(3.84/(self.attack_speed/100)) * min(self.targets, 2)
+
+		if self.talent_dmg and self.elite > 0:
+			summonhit = np.fmax(self.drone_atk - defense, self.drone_atk * 0.05)
+			dps += summonhit / self.drone_atk_interval
+
 		return dps
 
 class Whislash(Operator):
