cd4bcb73fd7d17f28cda3a514e21408035cc30fb
WhoAteMyCQQkie <128926564+WhoAteMyCQQkie@users.noreply.github.com>
2024-11-14 11:49:42 +0100

    Updated Carnelian

--- damagecalc/damage_formulas.py
+++ damagecalc/damage_formulas.py
@@ -1206,91 +1206,36 @@ class Caper(Operator):
 		return dps
 
 class Carnelian(Operator):
-	def __init__(self, pp, lvl = 0, pot=-1, skill=-1, mastery = 3, module=-1, module_lvl = 3, targets=1, TrTaTaSkMo=[True,True,True,True,True], buffs=[0,0,0],**kwargs):
-		maxlvl=90
-		lvl1atk = 807  #######including trust
-		maxatk = 926
-		self.atk_interval = 2   #### in seconds
-		level = lvl if lvl > 0 and lvl < maxlvl else maxlvl
-		self.base_atk = lvl1atk + (maxatk-lvl1atk) * (level-1) / (maxlvl-1)
-		self.pot = pot if pot in range(1,7) else 1
-		if self.pot > 3: self.base_atk += 34
-		
-		self.skill = skill if skill in [1,2,3] else 3 ###### check implemented skills
-		self.mastery = mastery if mastery in [0,1,2,3] else 3
-		if level != maxlvl: self.name = f"Carnelian Lv{level} P{self.pot} S{self.skill}" #####set op name
-		else: self.name = f"Carnelian P{self.pot} S{self.skill}"
-		if self.mastery == 0: self.name += "L7"
-		elif self.mastery < 3: self.name += f"M{self.mastery}"
-		self.targets = max(1,targets)
-		self.trait = TrTaTaSkMo[0]
-		self.talent1 = TrTaTaSkMo[1]
-		self.talent2 = TrTaTaSkMo[2]
-		self.skilldmg = TrTaTaSkMo[3]
-		self.moduledmg = TrTaTaSkMo[4]
-		
-		self.module = module if module in [0,1,2] else 1 ##### check valid modules
-		self.module_lvl = module_lvl if module_lvl in [1,2,3] else 3		
-		if level >= maxlvl-30:
-			if self.module == 1:
-				if self.module_lvl == 3: self.base_atk += 85
-				elif self.module_lvl == 2: self.base_atk += 76
-				else: self.base_atk += 66
-				self.name += f" ModX{self.module_lvl}"
-			elif self.module == 2:
-				if self.module_lvl == 3: self.base_atk += 95
-				elif self.module_lvl == 2: self.base_atk += 85
-				else: self.base_atk += 65
-				self.name += f" ModY{self.module_lvl}"
-			else: self.name += " no Mod"
-		else: self.module = 0
-		
-		if self.skilldmg and self.skill != 1: self.name += " charged"
+	def __init__(self, pp, *args, **kwargs):
+		super().__init__("Carnelian",pp,[1,2,3],[1,2],3,1,1)
+		if self.skill_dmg and self.skill != 1: self.name += " charged"
 		if self.skill == 3: self.name += " (averaged)"
-		
-		if self.moduledmg and self.module == 2: self.name += " manyTargets"
-		
+		if self.module_dmg and self.module == 2: self.name += " manyTargets"
 		if self.targets > 1: self.name += f" {self.targets}targets" ######when op has aoe
 		
-		self.buffs = buffs
-			
-	
 	def skill_dps(self, defense, res):
-		dps = 0
-		atkbuff = self.buffs[0]
-		aspd = self.buffs[2]
-		atk_scale = 1
-		
-		#talent/module buffs
-		if self.module == 2 and self.moduledmg: atk_scale = 1.15
-			
-		####the actual skills
+		atk_scale = 1.15 if self.module == 2 and self.module_dmg else 1
+
 		if self.skill == 1:
-			atkbuff += 0.6 if self.mastery == 3 else 0.4 + 0.05 * self.mastery
-
-			final_atk = self.base_atk * (1+atkbuff) + self.buffs[1]
-			
+			final_atk = self.atk * (1 + self.buff_atk + self.skill_params[0]) + self.buff_atk_flat
 			hitdmg = np.fmax(final_atk * atk_scale * (1-res/100), final_atk * atk_scale * 0.05)
-			dps = hitdmg/(self.atk_interval/(1+aspd/100)) * self.targets
+			dps = hitdmg/self.atk_interval * self.attack_speed/100 * self.targets
 		if self.skill == 2:
-			self.atk_interval = 0.9 if self.mastery == 3 else 1.2
-			if self.skilldmg: atkbuff += 0.2 if self.mastery > 1 else 0.15
-
-			final_atk = self.base_atk * (1+atkbuff) + self.buffs[1]
-			
+			atk_interval = self.atk_interval + self.skill_params[0]
+			atkbuff = self.skill_params[2] if self.skill_dmg else 0
+			final_atk = self.atk * (1 + self.buff_atk + atkbuff) + self.buff_atk_flat
 			hitdmg = np.fmax(final_atk * atk_scale * (1-res/100), final_atk * atk_scale * 0.05)
-			dps = hitdmg/(self.atk_interval/(1+aspd/100)) * self.targets
+			dps = hitdmg/atk_interval * self.attack_speed/100 * self.targets
 		if self.skill == 3:
-			maxatkbuff = 2.8 if self.mastery == 3 else 2 + 0.2 * self.mastery
+			maxatkbuff = self.skill_params[0]
 			duration = 21
-			totalatks = 1 + int(duration / (self.atk_interval/(1+aspd/100))) # +1 because the first attack is already at 0
-			totalduration = totalatks * (self.atk_interval/(1+aspd/100))
+			totalatks = 1 + int(duration / (self.atk_interval/(self.attack_speed/100))) # +1 because the first attack is already at 0
+			totalduration = totalatks * (self.atk_interval/(self.attack_speed/100))
 			damage = 0
-			bonusscaling = 5 if self.skilldmg else 0
+			bonusscaling = 5 if self.skill_dmg else 0
 			for i in range(totalatks):
-				final_atk = self.base_atk * (1+atkbuff + i * (self.atk_interval/(1+aspd/100)) /21 * maxatkbuff) + self.buffs[1]
+				final_atk = self.atk * (1 + self.buff_atk + i * (self.atk_interval/(self.attack_speed/100)) /21 * maxatkbuff) + self.buff_atk_flat
 				damage += np.fmax(final_atk * atk_scale * (1-res/100), final_atk * atk_scale * 0.05) * (1+ min(bonusscaling,i) * 0.2)
-				
 			dps = damage/totalduration * self.targets
 		return dps
 
